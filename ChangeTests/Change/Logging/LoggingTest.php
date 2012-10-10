2a03761a28ecae2088ebbe9e97ae542a9bec1579
Gaël PORT <gael.port@rbs.fr>
2012-10-10 11:39:13 +0200

    [NEW] Add setPriority method.

--- ChangeTests/Change/Logging/LoggingTest.php
+++ ChangeTests/Change/Logging/LoggingTest.php
@@ -1,5 +1,5 @@
 <?php
-namespace Tests\Change\Application;
+namespace Tests\Change\Logging;
 
 class LoggingManagerTest extends \PHPUnit_Framework_TestCase
 {
@@ -7,23 +7,46 @@ class LoggingManagerTest extends \PHPUnit_Framework_TestCase
 	{
 		$application = \Change\Application::getInstance();
 		$config = $application->getApplicationServices()->getConfiguration();
+		$logging = $application->getApplicationServices()->getLogging();
 		
 		$config->addVolatileEntry('logging/level', 'ALERT');
-		$this->assertEquals('ALERT', $application->getApplicationServices()->getLogging()->getLevel());
+		$this->assertEquals('ALERT', $logging->getLevel());
 		$config->addVolatileEntry('logging/level', 'ERR');
-		$this->assertEquals('ERR', $application->getApplicationServices()->getLogging()->getLevel());
+		$this->assertEquals('ERR', $logging->getLevel());
 		
-		return $config;
+		return $logging;
 	}
 	
 	/**
 	 * @depends testGetLevel
 	 */
-	public function testGetPriority(\Change\Configuration\Configuration $config)
+	public function testGetAndSetPriority()
 	{
 		$application = \Change\Application::getInstance();
+		$config = $application->getApplicationServices()->getConfiguration();
+		$logging = $application->getApplicationServices()->getLogging();		
 		$config->addVolatileEntry('logging/level', 'DEBUG');
-		$this->assertEquals('DEBUG', $application->getApplicationServices()->getLogging()->getLevel());
-		$this->assertEquals(7, $application->getApplicationServices()->getLogging()->getPriority());
+		
+		// Setting valid value is OK.
+		$logging->setPriority(5);
+		$this->assertEquals(5, $logging->getPriority());
+		$logging->setPriority(2);
+		$this->assertEquals(2, $logging->getPriority());
+		
+		// Setting null calculate the value.
+		$config->addVolatileEntry('logging/level', 'DEBUG');
+		$logging->setPriority(null);
+		$this->assertEquals(7, $logging->getPriority());
+		$config->addVolatileEntry('logging/level', 'ERR');
+		$logging->setPriority(null);
+		$this->assertEquals(3, $logging->getPriority());
+		
+		// Setting invalid value reset it to calculated one.
+		$config->addVolatileEntry('logging/level', 'NOTICE');
+		$logging->setPriority(15);
+		$this->assertEquals(5, $logging->getPriority());
+		$config->addVolatileEntry('logging/level', 'INFO');
+		$logging->setPriority('toto');
+		$this->assertEquals(6, $logging->getPriority());
 	}
 }
