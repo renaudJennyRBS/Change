a1a1d91836e21c774d690cf05a22569ddbc207c9
Franck STAUFFER <franck.stauffer@rbs.fr>
2012-10-12 20:00:58 +0200

    Support for command groups and aliases

--- Change/Application/Console/ConsoleApplication.php
+++ Change/Application/Console/ConsoleApplication.php
@@ -2,8 +2,14 @@
 
 namespace Change\Application\Console;
 
+use Zend\Json\Json;
+use Change\Stdlib\Path;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Finder\Glob;
-
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Finder\Finder;
 
@@ -12,7 +18,7 @@ class ConsoleApplication extends \Symfony\Component\Console\Application
 	/**
 	 * @var array
 	 */
-	protected $cmdAliases;
+	protected $configuration;
 
 	/**
 	 * @var \Change\Application
@@ -28,6 +34,29 @@ class ConsoleApplication extends \Symfony\Component\Console\Application
 	}
 
 	/**
+	 * @return array
+	 */
+	public function getConfiguration()
+	{
+		if (!$this->configuration)
+		{
+			$globalConfig = array();
+			$projectConfig = array();
+			if (isset($_SERVER['HOME']) && file_exists($_SERVER['HOME'] . '/.console.json'))
+			{
+				$globalConfig = Json::decode(file_get_contents($_SERVER['HOME'] . '/.console.json'), Json::TYPE_ARRAY);
+			}
+			$projectConfigFile = Path::appPath('Config', 'console.json'); 
+			if (file_exists($projectConfigFile))
+			{
+				$projectConfig = Json::decode(file_get_contents($projectConfigFile), Json::TYPE_ARRAY);
+			}
+			$this->configuration = array_merge_recursive($globalConfig, $projectConfig);
+		}
+		return $this->configuration;
+	}
+
+	/**
 	 * @param \Change\Application $changeApplication
 	 */
 	public function setChangeApplication(\Change\Application $changeApplication)
@@ -40,14 +69,8 @@ class ConsoleApplication extends \Symfony\Component\Console\Application
 	 */
 	public function registerCommands()
 	{
-		// loads the potential shortcuts file
-		if (file_exists(PROJECT_HOME . '/aliases.json'))
-		{
-			$this->cmdAliases = json_decode(file_get_contents(PROJECT_HOME . '/aliases.json'), true);
-		}
-		
 		$changeCommandDir = new \SplFileInfo(PROJECT_HOME . '/Change/Commands');
-		$this->registerCommandsInDir($changeCommandDir);
+		$this->registerCommandsInDir($changeCommandDir, 'change');
 		
 		// Register project commands
 		$finder = new Finder();
@@ -70,6 +93,55 @@ class ConsoleApplication extends \Symfony\Component\Console\Application
 				$this->registerCommandsInDir($commandDir, strtolower($vendorName) . '-' . strtolower($moduleName), '\\' . $vendorName . '\\' . $moduleName . '\\' . 'Commands');
 			}
 		}
+		
+		$this->registerCommandGroups();
+		
+	}
+
+	/**
+	 * @throws \RuntimeException
+	 */
+	protected function registerCommandGroups()
+	{
+		$config = $this->getConfiguration();
+		$groups = isset($config['groups']) ? $config['groups'] : array();
+		foreach ($groups as $name => $commandNames)
+		{
+			$command = new ChangeCommand($name);
+			$application = $this;
+			$command->addOption('--isolation', null, InputOption::VALUE_NONE, 'Run commands in separate processes');
+			$command->addOption('--ignore-errors', null, InputOption::VALUE_NONE, 'Ignore subcommand error');
+			$command->setCode(function(InputInterface $input, OutputInterface $output) use ($commandNames, $application){
+		
+				$style = new OutputFormatterStyle('yellow', null, array('bold'));
+				$output->getFormatter()->setStyle('strong', $style);
+				foreach($commandNames as $commandName)
+				{
+					$output->writeln("");
+					$output->writeln("<strong>Executing command $commandName</strong>");
+					$output->writeln("");
+					if ($input->getOption('isolation'))
+					{
+						$process = new Process(PHP_CLI_PATH . ' ' . $_SERVER['argv'][0] . " $commandName ". ($input->getOption('dev') ? '--dev' : ''));
+						$process->run(function ($type, $buffer) use ($output) {
+							$output->write($buffer, false, OutputInterface::OUTPUT_RAW);
+						});
+					}
+					else
+					{
+						$command = $application->find($commandName);
+						$subCommandInput = new ArrayInput(array('command' => $commandName, '--dev' => $input->getOption('dev')));
+						$returnCode = $command->run($subCommandInput, $output);
+						if ($returnCode && !$this->getOption('ignore-errors'))
+						{
+							throw new \RuntimeException('Command ' . $commandName . ' failed', $returnCode);
+						}
+					}
+				}
+			});
+			$command->setChangeApplication($this->getChangeApplication());
+			$this->add($command);
+		}
 	}
 	
 	/**
@@ -78,6 +150,9 @@ class ConsoleApplication extends \Symfony\Component\Console\Application
 	 */
 	protected function registerCommandsInDir(\SplFileInfo $dir, $group = null, $namespace = null)
 	{
+		$config = $this->getConfiguration();
+		$aliases = isset($config['aliases']) ? $config['aliases'] : array();
+		
 		$cmdFinder = new Finder();
 		foreach ($cmdFinder->files()->depth('== 0')->in($dir->getPathname())->name('*.php') as $file)
 		{
@@ -95,12 +170,20 @@ class ConsoleApplication extends \Symfony\Component\Console\Application
 				{
 					$commandName =  $group . ':' . $commandName;  
 				}
-				/* @var $command \Change\Application\Console\AbstractCommand */
+				/* @var $command \Change\Application\Console\ChangeCommand */
 				$command = new $commandClassName($commandName);
-				if (isset($this->cmdAliases[$commandName]))
+				if (isset($aliases[$commandName]))
 				{
 					$currentAliases = $command->getAliases();
-					$currentAliases[] = $this->cmdAliases[$commandName];
+					if (is_array($aliases[$commandName]))
+					{
+						$currentAliases = array_merge($currentAliases, $aliases[$commandName]);
+					}
+					elseif (is_string($aliases[$commandName]))
+					{
+						$currentAliases[] = $aliases[$commandName];
+					}
+					
 					$command->setAliases($currentAliases);
 				}
 				$command->setChangeApplication($this->getChangeApplication());
