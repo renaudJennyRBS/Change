deb2a8946fbf2e44e721df58db9d15ffb81b8832
Eric HAUSWALD <eric.hauswald@rbs.fr>
2012-12-14 11:54:55 +0100

    [UPDATE] Move function from \SelectClause to \SelectQuery

--- Change/Db/Query/Clauses/SelectClause.php
+++ Change/Db/Query/Clauses/SelectClause.php
@@ -22,136 +22,11 @@ class SelectClause extends AbstractClause
 	protected $selectList;
 	
 	/**
-	 *
-	 * @var \Change\Db\Query\Clauses\FromClause
-	 */
-	protected $fromClause;
-	
-	/**
-	 *
-	 * @var \Change\Db\Query\Clauses\WhereClause
-	 */
-	protected $whereClause;
-	
-	/**
-	 *
-	 * @var \Change\Db\Query\Clauses\HavingClause
-	 */
-	protected $havingClause;
-	
-	/**
-	 *
-	 * @var \Change\Db\Query\Clauses\OrderByClause
-	 */
-	protected $orderByClause;
-	
-	/**
-	 * @var \Change\Db\Query\Clauses\GroupByClause
-	 */
-	protected $groupByClause;
-	
-	/**
 	 * @param \Change\Db\Query\Expressions\ExpressionList $list
-	 * @param \Change\Db\Query\Clauses\FromClause $from
-	 * @param \Change\Db\Query\Clauses\WhereClause $where
-	 * @param \Change\Db\Query\Clauses\HavingClause $having
-	 * @param \Change\Db\Query\Clauses\OrderByClause $order
-	 * @param \Change\Db\Query\Clauses\GroupByClause $group
 	 */
-	public function __construct(Change\Db\Query\Expressions\ExpressionList $list = null,
-		Change\Db\Query\Clauses\FromClause $from = null,
-		Change\Db\Query\Clauses\WhereClause $where = null,
-		Change\Db\Query\Clauses\HavingClause $having = null,
-		Change\Db\Query\Clauses\OrderByClause $order = null,
-		Change\Db\Query\Clauses\GroupByClause $group = null)
+	public function __construct(Change\Db\Query\Expressions\ExpressionList $list = null)
 	{
 		if ($list) $this->setSelectList($list);
-		if ($from) $this->setFromClause($from);
-		if ($where) $this->setWhereClause($where);
-		if ($having) $this->setHavingClause($having);
-		if ($order) $this->setOrderByClause($order);
-		if ($group) $this->setGroupByClause($group);
-	}
-	
-	/**
-	 * @return \Change\Db\Query\Clauses\FromClause
-	 */
-	public function getFromClause()
-	{
-		return $this->fromClause;
-	}
-	
-	/**
-	 * @param \Change\Db\Query\Clauses\FromClause $fromClause
-	 */
-	public function setFromClause(\Change\Db\Query\Clauses\FromClause $fromClause)
-	{
-		$this->fromClause = $fromClause;
-	}
-	
-	/**
-	 * @return \Change\Db\Query\Clauses\WhereClause
-	 */
-	public function getWhereClause()
-	{
-		return $this->whereClause;
-	}
-	
-	/**
-	 * @param \Change\Db\Query\Clauses\WhereClause $whereClause
-	 */
-	public function setWhereClause(\Change\Db\Query\Clauses\WhereClause $whereClause)
-	{
-		$this->whereClause = $whereClause;
-	}
-	
-	/**
-	 * @return \Change\Db\Query\Clauses\HavingClause
-	 */
-	public function getHavingClause()
-	{
-		return $this->havingClause;
-	}
-	
-	/**
-	 * @param \Change\Db\Query\Clauses\HavingClause $havingClause
-	 */
-	public function setHavingClause(\Change\Db\Query\Clauses\HavingClause $havingClause)
-	{
-		$this->havingClause = $havingClause;
-	}
-	
-	/**
-	 * @return \Change\Db\Query\Clauses\OrderByClause
-	 */
-	public function getOrderByClause()
-	{
-		return $this->orderByClause;
-	}
-	
-	/**
-	 * @param \Change\Db\Query\Clauses\OrderByClause $orderByClause
-	 */
-	public function setOrderByClause(\Change\Db\Query\Clauses\OrderByClause $orderByClause)
-	{
-		$this->orderByClause = $orderByClause;
-	}
-	
-	/**
-	 *
-	 * @return \Change\Db\Query\Clauses\GroupByClause
-	 */
-	public function getGroupByClause()
-	{
-		return $this->groupByClause;
-	}
-	
-	/**
-	 * @param \Change\Db\Query\Clauses\GroupByClause $groupByClause
-	 */
-	public function setGroupByClause(\Change\Db\Query\Clauses\GroupByClause $groupByClause)
-	{
-		$this->groupByClause = $groupByClause;
 	}
 	
 	/**
@@ -175,6 +50,10 @@ class SelectClause extends AbstractClause
 	 */
 	public function addSelect($expression)
 	{
+		if ($this->selectList === null)
+		{
+			$this->selectList = new \Change\Db\Query\Expressions\ExpressionList();
+		}
 		$this->selectList->add($expression);
 	}
 	
@@ -211,29 +90,6 @@ class SelectClause extends AbstractClause
 		}
 		
 		$parts[] = $selectList->toSQL92String();
-		
-		$fromClause = $this->getFromClause();
-		if ($fromClause)
-		{
-			$parts[] = $fromClause->toSQL92String();
-		}
-		$whereClause = $this->getWhereClause();
-		if ($whereClause)
-		{
-			$parts[] = $whereClause->toSQL92String();
-		}
-		
-		$groupByClause = $this->getGroupByClause();
-		if ($groupByClause)
-		{
-			$parts[] = $groupByClause->toSQL92String();
-		}
-		
-		$orderByClause = $this->getOrderByClause();
-		if ($orderByClause)
-		{
-			$parts[] = $orderByClause->toSQL92String();
-		}
 		return implode(' ', $parts);
 	}
 }
