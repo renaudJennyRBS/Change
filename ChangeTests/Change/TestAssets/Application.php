3bd8d89a77af38fc763cf5b83fd90f43a54124d7
Franck STAUFFER <franck.stauffer@rbs.fr>
2013-02-05 19:43:36 +0100

    Bad Gitignore file

---
+++ ChangeTests/Change/TestAssets/Application.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ChangeTests\Change\TestAssets;
+
+/**
+ * @name \ChangeTests\Change\TestAssets\Application
+ */
+class Application extends \Change\Application
+{
+	public function getWorkspace()
+	{
+		if (!$this->workspace)
+		{
+			$this->workspace = new \ChangeTests\Change\TestAssets\UnitTestWorkspace();
+		}
+		return $this->workspace;
+	}
+
+	public function registerCoreAutoload()
+	{
+		parent::registerCoreAutoload();
+		$zendLoader  = new \Zend\Loader\StandardAutoloader();
+		$zendLoader->registerNamespace('ChangeTests', dirname(dirname(__DIR__)));
+		$zendLoader->register();
+	}
+}
