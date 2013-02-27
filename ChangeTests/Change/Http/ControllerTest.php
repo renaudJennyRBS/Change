e3fa76a386211d1c686fbfd313310f8eb208ac83
Eric HAUSWALD <eric.hauswald@rbs.fr>
2013-02-27 10:32:16 +0100

    Add Change\Http\Rest tests

--- ChangeTests/Change/Http/ControllerTest.php
+++ ChangeTests/Change/Http/ControllerTest.php
@@ -2,6 +2,7 @@
 namespace ChangeTests\Change\Http;
 
 use Change\Http\Controller;
+use ChangeTests\Change\Http\TestAssets\FakeEventHandler;
 
 class ControllerTest extends \ChangeTests\Change\TestAssets\TestCase
 {
@@ -109,98 +110,5 @@ class ControllerTest extends \ChangeTests\Change\TestAssets\TestCase
 		$this->assertEquals(array('onRequest', 'onAction', 'execute', 'onResult', 'onResponse'), $fakeEventHandler->callNames);
 
 		return $controller;
-	}
-}
-
-class FakeEventHandler
-{
-	public $callNames = array();
-
-	public $throwOn;
-
-	public function setThrowOn($throwOn)
-	{
-		$this->throwOn = $throwOn;
-		$this->callNames = array();
-	}
-
-	/**
-	 * @param \Change\Http\Event $event
-	 * @throws \RuntimeException
-	 */
-	public function onRequest($event)
-	{
-		$this->callNames[] = 'onRequest';
-		if ($this->throwOn == 'onRequest')
-		{
-			throw new \RuntimeException('onRequest', 10000);
-		}
-	}
-
-	/**
-	 * @param \Change\Http\Event $event
-	 * @throws \RuntimeException
-	 */
-	public function onAction($event)
-	{
-		$this->callNames[] = 'onAction';
-		$event->setAction(array($this, 'execute'));
-		if ($this->throwOn == 'onAction')
-		{
-			throw new \RuntimeException('onAction', 10001);
-		}
-	}
-
-	/**
-	 * @param \Change\Http\Event $event
-	 * @throws \RuntimeException
-	 */
-	public function onResult($event)
-	{
-		$this->callNames[] = 'onResult';
-		if ($this->throwOn == 'onResult')
-		{
-			throw new \RuntimeException('onResult', 10002);
-		}
-	}
-
-	/**
-	 * @param \Change\Http\Event $event
-	 * @throws \RuntimeException
-	 */
-	public function onResponse($event)
-	{
-		$this->callNames[] = 'onResponse';
-		if ($this->throwOn == 'onResponse')
-		{
-			throw new \RuntimeException('onResponse', 10003);
-		}
-	}
-
-	/**
-	 * @param \Change\Http\Event $event
-	 * @throws \RuntimeException
-	 */
-	public function onException($event)
-	{
-		$code =  $event->getParam('Exception')->getCode();
-		$this->callNames[] = 'onException(' . $code . ')';
-		if ($this->throwOn == 'onException')
-		{
-			throw new \RuntimeException('onException', 10004);
-		}
-	}
-
-	/**
-	 * @param \Change\Http\Event $event
-	 * @throws \RuntimeException
-	 */
-	public function execute($event)
-	{
-		$this->callNames[] = 'execute';
-		if ($this->throwOn == 'execute')
-		{
-			throw new \RuntimeException('execute', 10005);
-		}
 	}
 }
