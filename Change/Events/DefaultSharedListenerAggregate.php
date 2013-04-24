70c1ef74abce9fae4fb4d8012fee9fb3632bc8ee
Gaël PORT <gael.port@rbs.fr>
2013-04-24 09:49:35 +0200

    [NEW] Add events on text formatting and key not found.

--- Change/Events/DefaultSharedListenerAggregate.php
+++ Change/Events/DefaultSharedListenerAggregate.php
@@ -14,38 +14,34 @@ class DefaultSharedListenerAggregate implements SharedListenerAggregateInterface
 {
 	/**
 	 * Attach one or more listeners
-	 *
 	 * Implementors may add an optional $priority argument; the SharedEventManager
 	 * implementation will pass this to the aggregate.
-	 *
 	 * @param SharedEventManagerInterface $events
 	 */
 	public function attachShared(SharedEventManagerInterface $events)
 	{
 		$identifiers = array('Documents');
 
-		$callBack = function($event) {
+		$callBack = function ($event)
+		{
 			$l = new \Change\Documents\Events\ValidateListener();
 			$l->onValidate($event);
 		};
-
 		$events->attach($identifiers, array(DocumentEvent::EVENT_CREATE, DocumentEvent::EVENT_UPDATE), $callBack, 5);
 
-		$callBack = function($event) {
+		$callBack = function ($event)
+		{
 			$l = new \Change\Documents\Events\DeleteListener();
 			$l->onDelete($event);
 		};
-
 		$events->attach($identifiers, DocumentEvent::EVENT_DELETE, $callBack, 5);
 	}
 
 	/**
 	 * Detach all previously attached listeners
-	 *
 	 * @param SharedEventManagerInterface $events
 	 */
 	public function detachShared(SharedEventManagerInterface $events)
 	{
-
 	}
 }
