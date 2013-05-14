bdd1d0865719dfe6324634cdacdf55652cc829c7
Frédéric Bonjour <frederic.bonjour@rbs.fr>
2013-05-14 17:29:28 +0200

    Added drag and drop support to reorder the items in TokenList.

--- Plugins/Modules/Change/Admin/Assets/js/directives/token-list.js
+++ Plugins/Modules/Change/Admin/Assets/js/directives/token-list.js
@@ -2,36 +2,36 @@
 
 	"use strict";
 
-	var app = angular.module('RbsChange');
+	var	app = angular.module('RbsChange');
 
-	app.directive('tokenList', ['RbsChange.ArrayUtils', '$filter', '$parse', function (Array, $filter, $parse) {
-
-		var counter = 0;
+	app.directive('tokenList', ['RbsChange.ArrayUtils', '$filter', '$parse', function (ArrayUtils, $filter) {
 
 		return {
 			restrict : 'E',
-
-			template: '<ul class="token-list">' +
-			'<li draggable="true" ng-repeat="item in items" id="tokenList{{counter}}-item{{item.id}}" ng-click="itemClicked($index, $event)" ng-class="{selected: item.$selected}">' +
-			'<a ng-hide="readonly" href="javascript:;" class="delete" ng-click="remove($index)">&times;</a><span ng-bind-html="getItemLabel(item)"></span>' +
-			'</li>' +
-			'</ul>',
-
-			replace: true,
+			replace  : true,
+			template :
+				'<ul class="token-list">' +
+					'<li draggable="true" ng-repeat="item in items" data-id="{{item.id}}" ng-click="itemClicked($index, $event)" ng-class="{selected: item.$selected}">' +
+						'<a ng-hide="readonly" href="javascript:;" class="delete" ng-click="remove($index)">&times;</a>{{ getItemLabel(item) }}' +
+						'<i class="pull-right icon-reorder icon-large" ng-hide="disableReordering"></i>' +
+					'</li>' +
+				'</ul>',
 
 			// Create isolated scope.
-			scope: {
+			scope : {
 				items: '='
 			},
 
 			link : function (scope, elm, attrs) {
 				var $el = $(elm),
 					lastSelectedItemIndex = -1,
-					labelProperty = attrs.labelProperty || 'label';
+					labelProperty = attrs.labelProperty || 'label',
+					dragging, isHandle, startIndex, stopIndex,
+					placeholder = $('<li class="sortable-placeholder"/>');
 
 				scope.readonly = attrs.readonly ? true : false;
+				scope.disableReordering = attrs.disableReordering ? true : false;
 
-				scope.counter = ++counter;
 
 				scope.getItemLabel = function (item) {
 					var val = item[labelProperty];
@@ -44,9 +44,7 @@
 								return item[property] || '';
 							}
 						});
-
 						val = val.replace(/(icon-[a-z\-]+)/g, '<i class="$1"></i>');
-
 					} else if (attrs.labelFilter) {
 						val = $filter(attrs.labelFilter)(val);
 					}
@@ -54,43 +52,11 @@
 					return val;
 				};
 
-				if ( ! scope.readonly ) {
-					$el.on('keydown', 'li a.delete', function (event) {
-						if (event.keyCode === 46 || event.keyCode === 8) { // delete or back key
-							scope.$apply(function () {
-								scope.deleteSelected();
-							});
-						} else if (! attrs.disableReordering && (event.keyCode === 38 || event.keyCode === 40)) {
-							var selected = -1, nb = 0;
-							for (var i = 0 ; i < scope.items.length ; i++) {
-								if (scope.items[i].$selected) {
-									selected = i;
-									nb++;
-								}
-							}
-							// Move items only if one is selected
-							if (nb === 1 && selected !== -1) {
-								if (event.keyCode === 38) { // top
-									if (selected > 0) {
-										Array.move(scope.items, selected, selected-1);
-										selected--;
-									}
-								} else if (event.keyCode === 40) { // bottom
-									if (selected < (scope.items.length-1)) {
-										Array.move(scope.items, selected, selected+1);
-										selected++;
-									}
-								}
-								scope.$apply();
-								$el.find('li:eq(' + selected + ') a.delete').focus();
-							}
-						}
-					});
-				}
 
 				scope.remove = function (index) {
-					Array.remove(scope.items, index);
+					ArrayUtils.remove(scope.items, index);
 				};
+
 
 				scope.itemClicked = function (index, event) {
 					if (scope.readonly) {
@@ -118,11 +84,13 @@
 					}
 				};
 
+
 				scope.clearSelected = function () {
 					angular.forEach(scope.items, function (item) {
 						item.$selected = false;
 					});
 				};
+
 
 				scope.deleteSelected = function () {
 					var i;
@@ -135,6 +103,112 @@
 						}
 					}
 				};
+
+
+				// Enable drag and drop to reorder the items.
+
+				if ( ! scope.disableReordering ) {
+
+					$(elm).on({
+						'mousedown': function() {
+							isHandle = true;
+						},
+						'mouseup': function() {
+							isHandle = false;
+						}
+					}, 'i.icon-reorder');
+
+					$(elm).on({
+						'dragstart': function (e) {
+							if (!isHandle) {
+								return false;
+							}
+							isHandle = false;
+							var dt = e.originalEvent.dataTransfer;
+							dt.effectAllowed = 'move';
+							dt.setData('Text', 'dummy');
+							dragging = $(this);
+							dragging.addClass('sortable-dragging');
+							startIndex = dragging.index();
+						},
+
+						'dragend': function () {
+							dragging.removeClass('sortable-dragging').show();
+							stopIndex = placeholder.index();
+							placeholder.detach();
+							if (stopIndex > startIndex) {
+								stopIndex--;
+							}
+							if (startIndex !== stopIndex) {
+								scope.$apply(function () {
+									ArrayUtils.move(scope.items, startIndex, stopIndex);
+								});
+							}
+							dragging = null;
+						}
+					}, 'li[data-id]');
+
+					$(elm).on({
+						'dragenter': function (e) {
+							e.preventDefault();
+							e.originalEvent.dataTransfer.dropEffect = 'move';
+						},
+
+						'dragover': function (e) {
+							e.preventDefault();
+							e.originalEvent.dataTransfer.dropEffect = 'move';
+
+							if (dragging) {
+								placeholder.height(dragging.height());
+								if ( ! $(this).is(placeholder) ) {
+									dragging.hide();
+									$(this)[placeholder.index() < $(this).index() ? 'after' : 'before'](placeholder);
+								}
+							}
+						},
+
+						'drop': function (e) {
+							e.stopPropagation();
+							placeholder.after(dragging);
+						}
+					}, 'li[data-id], li.sortable-placeholder');
+
+				}
+
+
+				if ( ! scope.readonly ) {
+					$el.on('keydown', 'li a.delete', function (event) {
+						if (event.keyCode === 46 || event.keyCode === 8) { // delete or back key
+							scope.$apply(function () {
+								scope.deleteSelected();
+							});
+						} else if (! attrs.disableReordering && (event.keyCode === 38 || event.keyCode === 40)) {
+							var selected = -1, nb = 0, i;
+							for (i = 0 ; i < scope.items.length ; i++) {
+								if (scope.items[i].$selected) {
+									selected = i;
+									nb++;
+								}
+							}
+							// Move items only if one is selected
+							if (nb === 1 && selected !== -1) {
+								if (event.keyCode === 38) { // top
+									if (selected > 0) {
+										ArrayUtils.move(scope.items, selected, selected-1);
+										selected--;
+									}
+								} else if (event.keyCode === 40) { // bottom
+									if (selected < (scope.items.length-1)) {
+										ArrayUtils.move(scope.items, selected, selected+1);
+										selected++;
+									}
+								}
+								scope.$apply();
+								$el.find('li:eq(' + selected + ') a.delete').focus();
+							}
+						}
+					});
+				}
 
 			}
 		};
