61e34228534554a170fc8ab69481168d7e79949c
Frédéric Bonjour <frederic.bonjour@rbs.fr>
2013-05-15 14:13:25 +0200

    Changed template file for documentListToolbar directive (twig).

--- Plugins/Modules/Change/Admin/Assets/js/directives/document-list-toolbar.js
+++ Plugins/Modules/Change/Admin/Assets/js/directives/document-list-toolbar.js
@@ -1,25 +1,26 @@
-(function ($) {
+(function () {
+
+	"use strict";
+
 	var app = angular.module('RbsChange');
 
-	app.directive('documentListToolbar', ['$timeout', function ($timeout) {
+	app.directive('documentListToolbar', function () {
 
 		var counter = 0;
 
 		return {
-			// Utilisation : <document-list-toolbar/>
-			restrict: 'E',
+			restrict    : 'E',
+			replace     : true,
+			templateUrl : 'Change/Admin/js/directives/document-list-toolbar.twig',
 
-			// URL du template HTML
-			templateUrl: 'Change/Admin/js/directives/document-list-toolbar.html',
-
-			replace: true,
 
 			// Initialisation du scope (logique du composant)
-			link: function (scope, element, attrs) {
+			link : function documentListToolbarLinkFn (scope, element, attrs) {
 				scope.thumbnailsViewEnabled = attrs.thumbnails;
 				scope.embeddedActionsOptionsContainerId = 'EAOC_' + (++counter);
 			}
 
 		};
-	}]);
-})(window.jQuery);
+	});
+
+})();
