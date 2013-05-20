e983b89af7f604d05bfb022c03235e5e8ef31579
Frédéric Bonjour <frederic.bonjour@rbs.fr>
2013-05-20 14:53:58 +0200

    Breadcrumb localization.

--- Plugins/Modules/Change/Admin/Assets/i18n.js
+++ Plugins/Modules/Change/Admin/Assets/i18n.js
@@ -2,7 +2,8 @@ __change.i18n = {
 	"m.change.admin.admin.js": {
 		"last-modification-date": "dernière modif.",
 		"time-zone-selector-title" : "sélection d'un fuseau horaire",
-		"confirm": "confirmation"
+		"confirm": "confirmation",
+		"home": "accueil"
 	},
 	"m.change.website.admin.js": {
 		"host-name": "nom d'hôte",
