28395038b160babf1fae6736848ca4ac926c2dc3
Frédéric Bonjour <frederic.bonjour@rbs.fr>
2013-05-23 11:07:06 +0200

    Added Themes list and editor in Theme plugin. PageTemplates are now listed per Theme, as expected.

--- Plugins/Modules/Change/Theme/Admin/Assets/js/admin.js
+++ Plugins/Modules/Change/Theme/Admin/Assets/js/admin.js
@@ -11,14 +11,13 @@
 		. when(
 			'/Change/Theme',
 			{
-				templateUrl : 'Change/Theme/PageTemplate/list.twig',
-				reloadOnSearch : false
+				redirectTo: '/Change/Theme/Theme'
 			})
 
-		// Website
+		// PageTemplate
 
 		. when(
-			'/Change/Theme/PageTemplate',
+			'/Change/Theme/Theme/:theme/Templates',
 			{
 				templateUrl : 'Change/Theme/PageTemplate/list.twig',
 				reloadOnSearch : false
@@ -29,10 +28,31 @@
 				templateUrl : 'Change/Theme/PageTemplate/form.twig',
 				reloadOnSearch : false
 			})
-			. when(
+		. when(
 			'/Change/Theme/PageTemplate/:id',
 			{
 				templateUrl : 'Change/Theme/PageTemplate/form.twig',
+				reloadOnSearch : false
+			})
+
+		// Theme
+
+		. when(
+			'/Change/Theme/Theme',
+			{
+				templateUrl : 'Change/Theme/Theme/list.twig',
+				reloadOnSearch : false
+			})
+		. when(
+			'/Change/Theme/Theme/:id/:LCID',
+			{
+				templateUrl : 'Change/Theme/Theme/form.twig',
+				reloadOnSearch : false
+			})
+		. when(
+			'/Change/Theme/Theme/:id',
+			{
+				templateUrl : 'Change/Theme/Theme/form.twig',
 				reloadOnSearch : false
 			})
 		;
@@ -42,11 +62,19 @@
 	app.config(['$provide', function ($provide) {
 		$provide.decorator('RbsChange.UrlManager', ['$delegate', function ($delegate) {
 
-			// Pages
+			// PageTemplate
 			$delegate.register('Change_Theme_PageTemplate', {
 				'form'  : '/Change/Theme/PageTemplate/:id/:LCID',
 				'list'  : '/Change/Theme/PageTemplate/:LCID',
 				'i18n'  : '/Change/Theme/PageTemplate/:id/:LCID/translate-from/:fromLCID'
+			});
+
+			// Theme
+			$delegate.register('Change_Theme_Theme', {
+				'form'  : '/Change/Theme/Theme/:id/:LCID',
+				'list'  : '/Change/Theme/Theme/:LCID',
+				'i18n'  : '/Change/Theme/Theme/:id/:LCID/translate-from/:fromLCID',
+				'tree'  : '/Change/Theme/Theme/:id/Templates'
 			});
 
 			return $delegate;
