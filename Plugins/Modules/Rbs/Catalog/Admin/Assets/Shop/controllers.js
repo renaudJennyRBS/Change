5922efeebdf21a2a29c0f2860624a644b476343b
Frédéric Bonjour <frederic.bonjour@rbs.fr>
2013-06-07 14:53:36 +0200

    Fixed Controllers: they do not need to use DocumentList service anymore, the <document-list/> directive does the job.

--- Plugins/Modules/Rbs/Catalog/Admin/Assets/Shop/controllers.js
+++ Plugins/Modules/Rbs/Catalog/Admin/Assets/Shop/controllers.js
@@ -8,36 +8,22 @@
 	 * Controller for list.
 	 *
 	 * @param $scope
-	 * @param DocumentList
 	 * @param Breadcrumb
 	 * @param MainMenu
 	 * @param i18n
 	 * @constructor
 	 */
-	function ListController($scope, DocumentList, Breadcrumb, MainMenu, i18n)
+	function ListController($scope, Breadcrumb, MainMenu, i18n)
 	{
 		Breadcrumb.resetLocation([
 			[i18n.trans('m.rbs.catalog.admin.js.module-name | ucf'), "Rbs/Catalog"],
 			[i18n.trans('m.rbs.catalog.admin.js.shop-list | ucf'), "Rbs/Catalog/Shop"]
 		]);
 
-		var DL = DocumentList.initScope($scope, 'Rbs_Catalog_Shop');
-		DL.viewMode = 'list';
-		DL.sort.column = 'nodeOrder';
-		DL.sort.descending = false;
-
-		$scope.createActions = [
-			{ 'label': i18n.trans('m.rbs.catalog.admin.js.shop | ucf'), 'url': 'Rbs/Catalog/Shop/new', 'icon': 'file' }
-		];
-
-		// Configure DataTable columns
-		DL.columns.push({ id: 'modificationDate', label: i18n.trans('m.rbs.admin.admin.js.modification-date | ucf'), sortable: true });
-		DL.columns.push({ id: 'activated', label: i18n.trans('m.rbs.admin.admin.js.activated | ucf'), width: "90px", align: "center", sortable: true });
-
 		MainMenu.loadModuleMenu('Rbs_Catalog');
 	}
 
-	ListController.$inject = ['$scope', 'RbsChange.DocumentList', 'RbsChange.Breadcrumb', 'RbsChange.MainMenu', 'RbsChange.i18n'];
+	ListController.$inject = ['$scope', 'RbsChange.Breadcrumb', 'RbsChange.MainMenu', 'RbsChange.i18n'];
 	app.controller('Rbs_Catalog_Shop_ListController', ListController);
 
 	/**
