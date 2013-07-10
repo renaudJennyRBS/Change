37f030e235a6de8ff9e5c00e8f138d52b2123f75
Gaël PORT <gael.port@rbs.fr>
2013-07-10 10:09:16 +0200

    [FIX] Errors in product editor.

--- Plugins/Modules/Rbs/Catalog/Admin/Assets/Product/editor.js
+++ Plugins/Modules/Rbs/Catalog/Admin/Assets/Product/editor.js
@@ -21,62 +21,63 @@
 			scope: { original: '=document', onSave: '&', onCancel: '&', section: '=' },
 			link: function (scope, elm)
 			{
-				Editor.initScope(scope, elm);
+				Editor.initScope(scope, elm, function ()
+				{
+					// Categories.
+					scope.List = {selectedCondition: null};
+					Loading.start(i18n.trans('m.rbs.catalog.admin.js.condition-list-loading'));
+					REST.collection('Rbs_Catalog_Condition').then(function (conditions)
+					{
+						scope.List.conditions = conditions.resources;
+						/*for (var i = 5; i > 0; i--)
+						 {
+						 scope.List.conditions.unshift({id: i, label: 'toto' + i});
+						 }*/
+						scope.List.conditions.unshift({id: 0, label: i18n.trans('m.rbs.catalog.admin.js.no-condition')});
+						if (scope.List.conditions.length == 1)
+						{
+							scope.List.selectedCondition = scope.List.conditions[0];
+						}
+						Loading.stop();
+					});
+
+					scope.$watch('List.selectedCondition', function (newValue, oldValue)
+					{
+						if (newValue === oldValue || scope.document.isNew())
+						{
+							return;
+						}
+
+						var url = '';
+						if (newValue)
+						{
+							url = '/catalog/product/' + scope.document.id + '/' + scope.List.selectedCondition.id + '/categories/';
+						}
+						scope.categoryListUrl = url;
+					});
+
+					scope.addInCategories = function (docIds)
+					{
+						var conditionId = scope.List.selectedCondition.id;
+						var url = REST.getBaseUrl('catalog/product/' + scope.document.id + '/' + conditionId + '/categories/');
+						$http.put(url, {"addCategoryIds": docIds, "priorities": 0}, REST.getHttpConfig())
+							.success(function (data)
+							{
+								// TODO use data
+								scope.$broadcast('Change:DocumentList:DLRbsCatalogProductCategories:call', { 'method': 'reload' });
+							})
+							.error(function errorCallback(data, status)
+							{
+								data.httpStatus = status;
+								scope.$broadcast('Change:DocumentList:DLRbsCatalogProductCategories:call', { 'method': 'reload' });
+							});
+					};
+				});
 
 				// Prices.
 				scope.createActions = [
 					{ 'label': i18n.trans('m.rbs.catalog.admin.js.price | ucf'), 'url': 'Rbs/Catalog/Price/new', 'icon': 'file' }
 				];
-
-				// Categories.
-				scope.List = {selectedCondition: null};
-				Loading.start(i18n.trans('m.rbs.catalog.admin.js.condition-list-loading'));
-				REST.collection('Rbs_Catalog_Condition').then(function (conditions)
-				{
-					scope.List.conditions = conditions.resources;
-					/*for (var i = 5; i > 0; i--)
-					{
-						scope.List.conditions.unshift({id: i, label: 'toto' + i});
-					}*/
-					scope.List.conditions.unshift({id: 0, label: i18n.trans('m.rbs.catalog.admin.js.no-condition')});
-					if (scope.List.conditions.length == 1)
-					{
-						scope.List.selectedCondition = scope.List.conditions[0];
-					}
-					Loading.stop();
-				});
-
-				scope.$watch('List.selectedCondition', function (newValue, oldValue)
-				{
-					if (newValue === oldValue)
-					{
-						return;
-					}
-
-					var url = '';
-					if (newValue)
-					{
-						url = '/catalog/product/' + scope.document.id + '/' + scope.List.selectedCondition.id + '/categories/';
-					}
-					scope.categoryListUrl = url;
-				});
-
-				scope.addInCategories = function (docIds)
-				{
-					var conditionId = scope.List.selectedCondition.id;
-					var url = REST.getBaseUrl('catalog/product/' + scope.document.id + '/' + conditionId + '/categories/');
-					$http.put(url, {"addCategoryIds": docIds, "priorities": 0}, REST.getHttpConfig())
-						.success(function (data)
-						{
-							// TODO use data
-							scope.$broadcast('Change:DocumentList:DLRbsCatalogProductCategories:call', { 'method': 'reload' });
-						})
-						.error(function errorCallback(data, status)
-						{
-							data.httpStatus = status;
-							scope.$broadcast('Change:DocumentList:DLRbsCatalogProductCategories:call', { 'method': 'reload' });
-						});
-				};
 			}
 		};
 	}
