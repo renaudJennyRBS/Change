b96405e185c4d9d111e486acd8f9ed00bdb988bb
Frédéric Bonjour <frederic.bonjour@rbs.fr>
2013-07-12 16:52:31 +0200

    Added a bit of magic with the <rbs-kb-switch/> and rbs-kb-when="" attribute on descendants.

---
+++ Plugins/Modules/Rbs/Admin/Assets/js/services/keyboard.js
@@ -0,0 +1,108 @@
+(function ($) {
+
+	"use strict";
+
+	function KeyboardService ($rootScope) {
+
+		$rootScope.__changeKeyboardStatus = {
+			'shift' : false,
+			'ctrl' : false,
+			'alt' : false,
+			'meta' : false
+		};
+
+		function keydown (event) {
+			if (event.shiftKey) {
+				$rootScope.__changeKeyboardStatus.shift = true;
+			}
+			if (event.ctrlKey) {
+				$rootScope.__changeKeyboardStatus.ctrl = true;
+			}
+			if (event.altKey) {
+				$rootScope.__changeKeyboardStatus.alt = true;
+			}
+			if (event.metaKey) {
+				$rootScope.__changeKeyboardStatus.meta = true;
+			}
+			$rootScope.$digest();
+		}
+
+		function keyup (event) {
+			if (! event.shiftKey) {
+				$rootScope.__changeKeyboardStatus.shift = false;
+			}
+			if (! event.ctrlKey) {
+				$rootScope.__changeKeyboardStatus.ctrl = false;
+			}
+			if (! event.altKey) {
+				$rootScope.__changeKeyboardStatus.alt = false;
+			}
+			if (! event.metaKey) {
+				$rootScope.__changeKeyboardStatus.meta = false;
+			}
+			$rootScope.$digest();
+		}
+
+		this.watch = function (scope, key, callback) {
+			var expr = '__changeKeyboardStatus.' + key.replace(/\+/g, ' && __changeKeyboardStatus.').replace(/\-/g, ' && !__changeKeyboardStatus.');
+			var deregistrationFunc = $rootScope.$watch(expr, function (value) {
+				if (value === true || value === false) {
+					callback(value);
+				}
+			}, true);
+			scope.$on('$destroy', function () {
+				deregistrationFunc();
+			});
+		};
+
+		$('body').on('keydown', keydown);
+		$('body').on('keyup', keyup);
+	}
+
+
+	/**
+	 * <rbs-kb-switch>
+	 *    <i rbs-kb-when="shift-alt" class="icon-plane icon-3x"></i>
+	 *    <i rbs-kb-when="alt-shift" class="icon-bell icon-3x"></i>
+	 *    <i rbs-kb-when="alt+shift" class="icon-ambulance icon-3x"></i>
+	 *    <i rbs-kb-default="" class="icon-github icon-3x"></i>
+	 * </rbs-kb-switch>
+	 */
+	function rbsKbSwitch (Keyboard) {
+
+		return {
+			restrict : 'EAC',
+
+			link : function (scope, elm) {
+				var when, whenCounter, def;
+
+				when = elm.find('[rbs-kb-when]');
+				whenCounter = when.length;
+				def = elm.find('[rbs-kb-default]');
+
+				when.each(function () {
+					var $el = $(this);
+					Keyboard.watch(scope, $el.attr('rbs-kb-when'), function (value) {
+						if (value) {
+							def.hide();
+							$el.show();
+							whenCounter++;
+						} else {
+							$el.hide();
+							whenCounter--;
+							if (whenCounter === 0) {
+								def.show();
+							}
+						}
+					});
+
+				});
+			}
+		};
+	}
+
+	var app = angular.module('RbsChange');
+	app.service('RbsChange.Keyboard', ['$rootScope', KeyboardService]);
+	app.directive('rbsKbSwitch', ['RbsChange.Keyboard', rbsKbSwitch]);
+
+})( window.jQuery );
