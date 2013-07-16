9039ae2d6ad9d3cdc6a68be1f715d9a18aa53fc0
Frédéric Bonjour <frederic.bonjour@rbs.fr>
2013-07-16 18:06:46 +0200

    Prevent adding a function in a section if this function already exists.

--- Plugins/Modules/Rbs/Website/Documents/SectionPageFunction.php
+++ Plugins/Modules/Rbs/Website/Documents/SectionPageFunction.php
@@ -1,10 +1,41 @@
 <?php
 namespace Rbs\Website\Documents;
 
+use Change\Documents\Events\Event;
+
 /**
  * @name \Rbs\Website\Documents\SectionPageFunction
  */
 class SectionPageFunction extends \Compilation\Rbs\Website\Documents\SectionPageFunction
 {
+
+	/**
+	 * @param \Zend\EventManager\EventManagerInterface $eventManager
+	 */
+	protected function attachEvents($eventManager)
+	{
+		$eventManager->attach(Event::EVENT_CREATE, array($this, 'validateUnique'), 1);
+	}
+
+
+	/**
+	 * @param \Change\Documents\Events\Event $event
+	 */
+	public function validateUnique ($event)
+	{
+		if ($event->getParam('propertiesErrors') !== null)
+		{
+			return;
+		}
+
+		/* @var $document \Rbs\Website\Documents\SectionPageFunction */
+		$document = $event->getDocument();
+		$query = new \Change\Documents\Query\Query($document->getDocumentServices(), $document->getDocumentModel());
+		$query->andPredicates($query->eq('section', $document->getSection()), $query->eq('page', $document->getPage()), $query->eq('functionCode', $document->getFunctionCode()));
+		if ($query->getCountDocuments())
+		{
+			$event->setParam('propertiesErrors', array('section' => array(new \Change\I18n\PreparedKey('m.rbs.website.document.sectionpagefunction.error-not-unique'))));
+		}
+	}
 
 }
