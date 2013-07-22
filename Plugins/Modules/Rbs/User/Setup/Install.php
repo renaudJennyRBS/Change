bdb0b27898b6d4049edaedb1e5b87269388f208e
Renaud Jenny <renaud.jenny@rbs.fr>
2013-07-22 11:00:45 +0200

    [UPDATE] Move applications rest actions from Admin to User

--- Plugins/Modules/Rbs/User/Setup/Install.php
+++ Plugins/Modules/Rbs/User/Setup/Install.php
@@ -22,6 +22,7 @@ class Install
 
 		$config->addPersistentEntry('Change/Events/AuthenticationManager/Rbs_User', '\\Rbs\\User\\Events\\ListenerAggregate');
 		$config->addPersistentEntry('Change/Events/Http/Web/Rbs_User', '\\Rbs\\User\\Web\\ListenerAggregate');
+		$config->addPersistentEntry('Change/Events/Http/Rest/Rbs_User', '\\Rbs\\User\\Http\\Rest\\ListenerAggregate');
 	}
 
 	/**
