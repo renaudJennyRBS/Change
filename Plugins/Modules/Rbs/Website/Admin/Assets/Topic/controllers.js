b6c1019591e0c56c6b628c9bfcef7c6ed3505f2f
Frédéric Bonjour <frederic.bonjour@rbs.fr>
2013-07-22 14:16:02 +0200

    Added error handling.

--- Plugins/Modules/Rbs/Website/Admin/Assets/Topic/controllers.js
+++ Plugins/Modules/Rbs/Website/Admin/Assets/Topic/controllers.js
@@ -11,9 +11,12 @@
 	 * @param Breadcrumb
 	 * @param MainMenu
 	 * @param i18n
+	 * @param Query
+	 * @param Loading
+	 * @param NotificationCenter
 	 * @constructor
 	 */
-	function ListController($scope, $q, $location, Breadcrumb, MainMenu, i18n, REST, Query, Loading)
+	function ListController($scope, $q, $location, Breadcrumb, MainMenu, i18n, REST, Query, Loading, NotificationCenter)
 	{
 		Breadcrumb.resetLocation([
 			[i18n.trans('m.rbs.website.admin.js.module-name | ucf'), "Rbs/Website"]
@@ -51,16 +54,23 @@
 			$q.all([
 				REST.action('collectionItems', { 'code': 'Rbs_Website_AvailablePageFunctions' }),
 				REST.query(Query.simpleQuery('Rbs_Website_SectionPageFunction', 'section', $scope.currentSection.id), {'column': ['page', 'section', 'functionCode']})
-			]).then(function (results) {
-				$scope.loadingFunctions = false;
-				$scope.sectionFunctions = results[1].resources;
-				$scope.allFunctions = results[0].items;
-				$scope.allFunctions['Rbs_Website_Section'] = {"label": i18n.trans('m.rbs.website.admin.js.index-page | ucf')};
-				angular.forEach($scope.sectionFunctions, function (func) {
-					func.functionLabel = $scope.allFunctions[func.functionCode].label;
-				});
-				Loading.stop();
-			});
+			]).then(
+				function (results) {
+					$scope.loadingFunctions = false;
+					$scope.sectionFunctions = results[1].resources;
+					$scope.allFunctions = results[0].items;
+					$scope.allFunctions['Rbs_Website_Section'] = {"label": i18n.trans('m.rbs.website.admin.js.index-page | ucf')};
+					angular.forEach($scope.sectionFunctions, function (func) {
+						func.functionLabel = $scope.allFunctions[func.functionCode].label;
+					});
+					Loading.stop();
+				},
+				function (error) {
+					$scope.loadingFunctions = false;
+					NotificationCenter.error("Fonctions", error);
+					Loading.stop();
+				}
+			);
 		}
 
 		$scope.toggleFunctions = function ($event, show) {
@@ -80,7 +90,7 @@
 	ListController.$inject = [
 		'$scope', '$q', '$location',
 		'RbsChange.Breadcrumb', 'RbsChange.MainMenu', 'RbsChange.i18n', 'RbsChange.REST', 'RbsChange.Query',
-		'RbsChange.Loading'
+		'RbsChange.Loading', 'RbsChange.NotificationCenter'
 	];
 	app.controller('Rbs_Website_Topic_ListController', ListController);
 
