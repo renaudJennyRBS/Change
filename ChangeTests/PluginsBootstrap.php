22fddc764263f50e435768a97be9b6975da3600f
Eric HAUSWALD <eric.hauswald@rbs.fr>
2013-07-24 13:37:57 +0200

    Fix plugins autoload in tests

---
+++ ChangeTests/PluginsBootstrap.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Set error reporting to the level to which Zend Framework code must comply.
+ */
+error_reporting( E_ALL | E_STRICT );
+if (!defined('PROJECT_HOME'))
+{
+	define('PROJECT_HOME', dirname(realpath(__DIR__)));
+}
+require_once PROJECT_HOME . '/Change/Application.php';
+require_once 'Change/TestAssets/Application.php';
+
+$application = new \ChangeTests\Change\TestAssets\Application();
+$application->registerAutoload();
+$application->clearCache();
+$pluginManager = new \Change\Plugins\PluginManager();
+$pluginManager->setApplication($application);
+$pluginManager->compile(false);
+$application->registerPluginsAutoload();
