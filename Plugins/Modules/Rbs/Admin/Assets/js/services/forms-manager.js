1bec13a5e9dc4ee34c0d63047c1226c9faba84b4
Eric HAUSWALD <eric.hauswald@rbs.fr>
2013-07-24 09:51:52 +0200

    Fix angular Validators directives 'smartFloat' and 'integer'

--- Plugins/Modules/Rbs/Admin/Assets/js/services/forms-manager.js
+++ Plugins/Modules/Rbs/Admin/Assets/js/services/forms-manager.js
@@ -376,7 +376,7 @@
 			require : 'ngModel',
 			link : function (scope, elm, attrs, ctrl) {
 				ctrl.$parsers.unshift(function (viewValue) {
-					if (INTEGER_REGEXP.test(viewValue)) {
+					if (viewValue == '' || INTEGER_REGEXP.test(viewValue)) {
 						// it is valid
 						ctrl.$setValidity('integer', true);
 						return viewValue;
@@ -397,7 +397,7 @@
 			require : 'ngModel',
 			link : function (scope, elm, attrs, ctrl) {
 				ctrl.$parsers.unshift(function (viewValue) {
-					if (FLOAT_REGEXP.test(viewValue)) {
+					if (viewValue == '' || FLOAT_REGEXP.test(viewValue)) {
 						ctrl.$setValidity('float', true);
 						return parseFloat(viewValue.replace(',', '.'));
 					} else {
