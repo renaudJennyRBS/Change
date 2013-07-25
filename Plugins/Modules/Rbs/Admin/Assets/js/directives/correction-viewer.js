9c2556c9ff54ae37ffffdf28bec525da5f3ab20e
Frédéric Bonjour <frederic.bonjour@rbs.fr>
2013-07-25 09:46:45 +0200

    Cleanup.

--- Plugins/Modules/Rbs/Admin/Assets/js/directives/correction-viewer.js
+++ Plugins/Modules/Rbs/Admin/Assets/js/directives/correction-viewer.js
@@ -1,7 +1,10 @@
 (function () {
+
+	"use strict";
+
 	var app = angular.module('RbsChange');
 
-	app.directive('rbsCorrectionViewer', ['$timeout', 'RbsChange.Dialog', 'RbsChange.ArrayUtils', 'RbsChange.REST', 'RbsChange.Events', 'RbsChange.Utils', function ($timeout, Dialog, ArrayUtils, REST, Events, Utils) {
+	app.directive('rbsCorrectionViewer', ['RbsChange.Dialog', 'RbsChange.ArrayUtils', 'RbsChange.REST', 'RbsChange.Utils', function (Dialog, ArrayUtils, REST, Utils) {
 		return {
 
 			restrict    : 'A',
@@ -12,8 +15,6 @@
 			},
 
 			link : function (scope, element, attrs) {
-
-//				var originalDiff;
 
 				function update () {
 					ArrayUtils.clear(scope.diff);
@@ -33,57 +34,6 @@
 				scope.correctionInfo = angular.copy(scope.current.META$.correction);
 				update();
 
-/*
-				originalDiff = angular.copy(scope.diff);
-
-				scope.$watch('correctionInfo', update, true);
-
-
-				scope.cancelLine = function (line) {
-					//ArrayUtils.removeValue(scope.correctionInfo.propertiesNames, line.id);
-					Utils.removeCorrection(scope.current, line.id);
-				};
-
-
-				scope.hasChanges = function () {
-					return ! angular.equals(scope.diff, originalDiff);
-				};
-
-
-				scope.cancelChanges = function () {
-					scope.correctionInfo = angular.copy(scope.current.META$.correction);
-				};
-
-
-				scope.saveChanges = function () {
-
-					// Tell the Editor to update the document that is being edited.
-					// Changes will be made on the document: the "Save" button should be enabled so that the
-					// user can save the document. When the document is saved, all the properties whose value equals
-					// the value in the original document are removed from the correction object.
-					var properties = {};
-					console.log("diff=", originalDiff);
-					console.log("scope.correctionInfo.propertiesNames=", scope.correctionInfo.propertiesNames);
-					angular.forEach(originalDiff, function (line) {
-						if (ArrayUtils.inArray(line.id, scope.correctionInfo.propertiesNames) === -1) {
-							properties[line.id] = line.original;
-							ArrayUtils.removeValue(scope.current.META$.correction.propertiesNames, line.id);
-						}
-					});
-
-					if (scope.current.META$.correction.propertiesNames.length === 0) {
-						delete scope.current.META$.correction;
-					}
-
-					Dialog.closeEmbedded().then(function () {
-						// Notify the parent scope (bound to the FormsManager)
-						scope.$emit(Events.EditorCorrectionChanged, properties);
-					});
-
-				};
-*/
-
-				// TODO Init this according to the Correction info
 				scope.params = {
 					'applyCorrectionWhen' : scope.correctionInfo.publicationDate ? 'planned' : 'now',
 					'plannedCorrectionDate' : scope.correctionInfo.publicationDate
@@ -91,11 +41,8 @@
 
 				scope.deleteCorrection = function () {
 					Dialog.closeEmbedded().then(function () {
-						console.log("removing correction from ", scope.current);
-
 						var copy = angular.copy(scope.current);
 						if (Utils.removeCorrection(copy)) {
-							console.log("saving ", copy);
 							REST.save(copy).then(function (updated) {
 								delete scope.current.META$.correction;
 								angular.extend(scope.current, updated);
@@ -105,9 +52,6 @@
 						else {
 							console.warn("Could not remove Correction from ", copy);
 						}
-
-						// Notify the parent scope (bound to the FormsManager)
-						//scope.$emit(Events.EditorCorrectionRemoved);
 					});
 				};
 
@@ -134,7 +78,6 @@
 
 				function executeTask (taskCode) {
 					var params = {};
-					console.log("execute task: ", scope.params.applyCorrectionWhen, scope.params.plannedCorrectionDate);
 					if (scope.params.applyCorrectionWhen === 'planned') {
 						params['publicationDate'] = scope.params.plannedCorrectionDate;
 					}
