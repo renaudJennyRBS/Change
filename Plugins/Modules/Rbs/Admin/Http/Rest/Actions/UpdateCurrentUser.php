e5f7b91de42f0cf056eb3ccc95f8dcf69d3004d0
Frédéric Bonjour <frederic.bonjour@rbs.fr>
2013-07-30 16:06:37 +0200

    Added PUT "admin/currentUser" to save current user profile.

---
+++ Plugins/Modules/Rbs/Admin/Http/Rest/Actions/UpdateCurrentUser.php
@@ -0,0 +1,61 @@
+<?php
+namespace Rbs\Admin\Http\Rest\Actions;
+
+use Zend\Http\Response as HttpResponse;
+
+/**
+ * @name \Rbs\Admin\Http\Rest\Actions\UpdateCurrentUser
+ */
+class UpdateCurrentUser
+{
+
+	/**
+	 * @param \Change\Http\Event $event
+	 */
+	public function execute($event)
+	{
+		$user = $event->getAuthenticationManager()->getCurrentUser();
+		$profileManager = new \Change\User\ProfileManager();
+		$profileManager->setDocumentServices($event->getDocumentServices());
+
+		$props = $event->getRequest()->getPost()->toArray();
+
+		$profile = $profileManager->loadProfile($user, 'Change_User');
+		if ($profile)
+		{
+			$save = false;
+			foreach ($profile->getPropertyNames() as $name)
+			{
+				if (isset($props[$name]))
+				{
+					$profile->setPropertyValue($name, $props[$name]);
+					$save = true;
+				}
+			}
+			if ($save)
+			{
+				$profileManager->saveProfile($user, $profile);
+			}
+		}
+
+		$profile = $profileManager->loadProfile($user, 'Rbs_Admin');
+		if ($profile)
+		{
+			$save = false;
+			foreach ($profile->getPropertyNames() as $name)
+			{
+				if (isset($props[$name]))
+				{
+					$profile->setPropertyValue($name, $props[$name]);
+					$save = true;
+				}
+			}
+			if ($save)
+			{
+				$profileManager->saveProfile($user, $profile);
+			}
+		}
+
+		(new GetCurrentUser())->execute($event);
+	}
+}
