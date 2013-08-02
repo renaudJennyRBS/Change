a48a5a38894e6f497ba3e7585c175b8af009a603
Frédéric Bonjour <frederic.bonjour@rbs.fr>
2013-08-02 14:08:36 +0200

    Added directive on <time/> elements that allows display of relative dates.

--- Plugins/Modules/Rbs/Admin/Assets/js/rbschange.js
+++ Plugins/Modules/Rbs/Admin/Assets/js/rbschange.js
@@ -220,6 +220,89 @@
 	}]);
 
 
+	app.directive('time', ['$timeout', '$filter', function ($timeout, $filter) {
+
+		var DEFAULT_INTERVAL = 60;
+
+		return {
+			'restrict' : 'E',
+			'scope' : {
+				'datetime' : '@',
+				'display' : '@'
+			},
+
+			'link' : function (scope, element, attrs) {
+
+				if (! element.is('[datetime]')) {
+					throw new Error("Attribute 'datetime' is required on <time/> elements.");
+				}
+
+				var	dateTime,
+					stop,
+					content = element.html();
+
+				attrs.$observe('datetime', function (value) {
+					if (value) {
+						dateTime = moment(value);
+						if (stop) {
+							$timeout.cancel(stop);
+						}
+						update();
+					}
+				});
+
+				attrs.$observe('display', function () {
+					if (stop) {
+						$timeout.cancel(stop);
+					}
+					update();
+				});
+
+
+				function update () {
+					var html, title;
+
+					if (! dateTime) {
+						return;
+					}
+
+					switch (attrs.display) {
+						case 'relative':
+							html = dateTime.fromNow();
+							title = $filter('rbsDateTime')(dateTime.toDate());
+							break;
+						case 'both':
+							html = $filter('rbsDateTime')(dateTime.toDate()) + ' (' + dateTime.fromNow() + ')';
+							break;
+						default :
+							title = dateTime.fromNow();
+							html = $filter('rbsDateTime')(dateTime.toDate());
+					}
+
+					if (content) {
+						element.html(content.replace(/\{time\}/, html));
+					}
+					else {
+						element.html(html);
+					}
+
+					if (title) {
+						element.attr('title', title);
+					}
+
+					// Re-launch timer for next update
+					stop = $timeout(update, DEFAULT_INTERVAL*1000);
+				}
+
+				scope.$on('$destroy', function () {
+					$timeout.cancel(stop);
+				});
+			}
+		};
+	}]);
+
+
+
 	//-------------------------------------------------------------------------
 	//
 	// Controllers.
