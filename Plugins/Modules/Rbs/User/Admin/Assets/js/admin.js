6828e31e3edaf3f6e7839996d0b76f888c91f325
Frédéric Bonjour <frederic.bonjour@rbs.fr>
2013-08-08 14:32:14 +0200

    Added missing route for User profile.

--- Plugins/Modules/Rbs/User/Admin/Assets/js/admin.js
+++ Plugins/Modules/Rbs/User/Admin/Assets/js/admin.js
@@ -16,13 +16,9 @@
 				.route('permission'  , 'Rbs/User/User/:id/Permissions/' , 'Rbs/User/User/permission.twig')
 			;
 
-			$delegate.model('Rbs_User_Group')
-				.route('permission'  , 'Rbs/User/Group/:id/Permissions/', 'Rbs/User/User/permission.twig')
-			;
-
-			$delegate.model(null)
-				.route('home', 'Rbs/User/', { 'redirectTo': 'Rbs/User/User/'})
-			;
+			$delegate.model('Rbs_User_Group').route('permission'  , 'Rbs/User/Group/:id/Permissions/', 'Rbs/User/User/permission.twig');
+			$delegate.model(null).route('home', 'Rbs/User/', { 'redirectTo': 'Rbs/User/User/'});
+			$delegate.model(null).route('userProfile', 'Rbs/User/Profile', 'Rbs/User/Profile/profile.twig');
 
 			$delegate.routesForModels([
 				'Rbs_User_User',
