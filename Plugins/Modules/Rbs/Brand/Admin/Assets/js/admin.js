43fdcaa2d7df7275c656f5fc2be296b410322171
Gaël PORT <gael.port@rbs.fr>
2013-08-14 17:27:49 +0200

    [REFACTOR] Use simplified routes declarations.

--- Plugins/Modules/Rbs/Brand/Admin/Assets/js/admin.js
+++ Plugins/Modules/Rbs/Brand/Admin/Assets/js/admin.js
@@ -2,23 +2,16 @@
 {
 	var app = angular.module('RbsChange');
 
-	app.config(['$routeProvider', function ($routeProvider)
-	{
-		$routeProvider.when('/Rbs/Brand', { templateUrl: 'Rbs/Brand/Brand/list.twig', reloadOnSearch: false })
-			.when('/Rbs/Brand/Brand/', { templateUrl: 'Rbs/Brand/Brand/list.twig', reloadOnSearch: false })
-			.when('/Rbs/Brand/Brand/:id/:LCID', { templateUrl: 'Rbs/Brand/Brand/form.twig', reloadOnSearch: false })
-			.when('/Rbs/Brand/Brand/:id', { templateUrl: 'Rbs/Brand/Brand/form.twig', reloadOnSearch: false });
-	}]);
-
 	app.config(['$provide', function ($provide)
 	{
 		$provide.decorator('RbsChange.UrlManager', ['$delegate', function ($delegate)
 		{
-			$delegate.register('Rbs_Brand_Brand', {
-				'form': '/Rbs/Brand/Brand/:id/:LCID',
-				'list': '/Rbs/Brand/Brand/:LCID',
-				'i18n': '/Rbs/Brand/Brand/:id/:LCID/translate-from/:fromLCID'
-			});
+			$delegate.model(null)
+				.route('home', 'Rbs/Brand/', { 'redirectTo': 'Rbs/Brand/Brand/'});
+
+			$delegate.routesForLocalizedModels([
+				'Rbs_Brand_Brand'
+			]);
 			return $delegate;
 		}]);
 	}]);
