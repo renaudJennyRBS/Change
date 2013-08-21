c5277cd396ef8de394714d2503677224dc41798f
Frédéric Bonjour <frederic.bonjour@rbs.fr>
2013-08-21 12:29:03 +0200

    Fixed navigation in categories, routes and URL.

--- Plugins/Modules/Rbs/Catalog/Admin/Assets/js/admin.js
+++ Plugins/Modules/Rbs/Catalog/Admin/Assets/js/admin.js
@@ -11,23 +11,19 @@
 	{
 		$provide.decorator('RbsChange.UrlManager', ['$delegate', function ($delegate)
 		{
-			$delegate.model('Rbs_Catalog_Product')
-				.route('prices', 'Rbs/Catalog/Product/:id/Prices/', 'Rbs/Catalog/Product/product-prices.twig');
-
+			$delegate.model('Rbs_Catalog_Product').route('prices', 'Rbs/Catalog/Product/:id/Prices/', 'Rbs/Catalog/Product/product-prices.twig');
 			$delegate.model('Rbs_Catalog_Category')
-				.route('productcategorizations', 'Rbs/Catalog/Category/:id/ProductCategorization/', 'Rbs/Catalog/Category/products.twig');
+				.route('productcategorizations', 'Rbs/Catalog/Category/:id/ProductCategorization/', 'Rbs/Catalog/Category/products.twig')
+				.route('tree', 'Rbs/Catalog/nav/?tn=:id', 'Rbs/Catalog/Category/list.twig');
 
-
-			$delegate.model(null)
-				.route('home', 'Rbs/Catalog/', { 'redirectTo': 'Rbs/Catalog/Product/'})
-				.route('nav', '/Rbs/Catalog/nav/', 'Rbs/Catalog/Category/list.twig');
+			$delegate.model(null).route('home', 'Rbs/Catalog/', { 'redirectTo': 'Rbs/Catalog/Product/'});
 
 			$delegate.routesForLocalizedModels([
 				'Rbs_Catalog_Product',
 				'Rbs_Catalog_Category',
 				'Rbs_Catalog_Attribute'
 			]);
-			
+
 			return $delegate;
 		}]);
 
