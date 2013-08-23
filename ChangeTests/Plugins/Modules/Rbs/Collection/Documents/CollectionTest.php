8ef8d96bea63395fd56234701ce6fe3916ca051b
Renaud Jenny <renaud.jenny@rbs.fr>
2013-08-23 08:53:28 +0200

    [UPDATE] Add Unit Tests on documents for Module Collection

--- ChangeTests/Plugins/Modules/Rbs/Collection/Documents/CollectionTest.php
+++ ChangeTests/Plugins/Modules/Rbs/Collection/Documents/CollectionTest.php
@@ -65,4 +65,179 @@ class CollectionTest extends \ChangeTests\Change\TestAssets\TestCase
 		$collection->setLabel('RbsCollectionTestFailed');
 		$collection->save();
 	}
+
+	public function testOnUpdate()
+	{
+		//first create a non locked collection and update it
+		$collection1 = $this->createACollection('collection1', 5);
+		$this->assertFalse($collection1->getLocked());
+		$itemIds = $collection1->getItemsIds();
+		$code = $collection1->getCode();
+		//now we replace items and code
+		$collection1->setItems($this->createItems('reTest', 5));
+		$collection1->setCode('newCollection1');
+		$tm = $this->getApplicationServices()->getTransactionManager();
+		try
+		{
+			$tm->begin();
+			$collection1->update();
+			$tm->commit();
+		}
+		catch (\Exception $e)
+		{
+			$tm->rollBack($e);
+			$this->fail('cannot update collection with this error: ' . $e->getMessage());
+		}
+		$dm = $this->getDocumentServices()->getDocumentManager();
+		$collection1 = $dm->getDocumentInstance($collection1->getId());
+		$this->assertNotEquals($code, $collection1->getCode());
+		$this->assertNotEquals($itemIds, $collection1->getItemsIds());
+
+		//now we do the same test but with a locked collection
+		$collection2 = $this->createACollection('collection2', 5, true);
+		$this->assertTrue($collection2->getLocked());
+		$itemIds = $collection2->getItemsIds();
+		$code = $collection2->getCode();
+		//We change the code but that wont work because collection is locked
+		$collection2->setCode('newCollection2');
+		//now we add items, that will work because we just add one
+		$newItem = $this->createItems('reTest', 1)[0];
+		$newItems = $collection2->getItems()->add($newItem);
+		$collection2->setItems($newItems->toArray());
+		try
+		{
+			$tm->begin();
+			$collection2->update();
+			$tm->commit();
+		}
+		catch (\Exception $e)
+		{
+			$tm->rollBack($e);
+			$this->fail('cannot update collection with this error: ' . $e->getMessage());
+		}
+		$dm = $this->getDocumentServices()->getDocumentManager();
+		/* @var $collection2 \Rbs\Collection\Documents\Collection */
+		$collection2 = $dm->getDocumentInstance($collection2->getId());
+		$this->assertEquals($code, $collection2->getCode());
+		//Items are not equals because we add one
+		$this->assertNotEquals($itemIds, $collection2->getItemsIds());
+		//We check if this is the good one we added
+		$itemIdsDiff = array_diff($collection2->getItemsIds(), $itemIds);
+		$this->assertCount(1, $itemIdsDiff);
+		$this->assertEquals($newItem->getId(), array_pop($itemIdsDiff));
+
+		//next we check if we try to delete an element from items that doesn't work (because it's locked)
+		//keep item ids to compare after update and remove one of them
+		$itemIds = $collection2->getItemsIds();
+		$newItems = $collection2->getItems();
+		$count = $newItems->count();
+		$newItems->rewind();
+		$newItems->remove($newItems->current());
+		$this->assertNotEquals($count, $newItems->count());
+		try
+		{
+			$tm->begin();
+			//update will throw an exception because we cannot remove an item when a collection is locked
+			$this->setExpectedException('\RuntimeException', 'can not removed locked item from collection');
+			$collection2->update();
+			$tm->commit();
+		}
+		catch (\Exception $e)
+		{
+			$tm->rollBack($e);
+		}
+
+	}
+
+	public function testOnDelete()
+	{
+		//first create a non locked collection and delete it
+		$collection1 = $this->createACollection('collectionToDelete', 5);
+		$this->assertFalse($collection1->getLocked());
+		$itemIds = $collection1->getItemsIds();
+		$tm = $this->getApplicationServices()->getTransactionManager();
+		try
+		{
+			$tm->begin();
+			$collection1->delete();
+			$tm->commit();
+		}
+		catch (\Exception $e)
+		{
+			$tm->rollBack($e);
+			$this->fail('cannot delete collection with this error: ' . $e->getMessage());
+		}
+		$dm = $this->getDocumentServices()->getDocumentManager();
+//		$collection = $dm->getDocumentInstance($collection1->getId());
+		//check if items are all been deleted
+		foreach ($itemIds as $itemId)
+		{
+//			$this->assertNull($dm->getDocumentInstance($itemId));
+		}
+
+	}
+
+	/**
+	 * @param string $collectionCode
+	 * @param integer $numberOfItems
+	 * @param boolean $locked
+	 * @return \Rbs\Collection\Documents\Collection
+	 */
+	protected function createACollection($collectionCode, $numberOfItems, $locked = false)
+	{
+		$dm = $this->getDocumentServices()->getDocumentManager();
+		$tm = $this->getApplicationServices()->getTransactionManager();
+		$items = $this->createItems('test', 5);
+
+		$collection = $dm->getNewDocumentInstanceByModelName('Rbs_Collection_Collection');
+		/* @var $collection \Rbs\Collection\Documents\Collection */
+		$collection->setCode($collectionCode);
+		$collection->setLabel('rbs' . ucfirst($collectionCode));
+		$collection->setItems($items);
+		$collection->setLocked($locked);
+		try
+		{
+			$tm->begin();
+			$collection->save();
+			$tm->commit();
+		}
+		catch (\Exception $e)
+		{
+			$tm->rollBack($e);
+			$this->fail('cannot create collection ' . $collectionCode . ' with this error: ' . $e->getMessage());
+		}
+		return $collection;
+	}
+
+	/**
+	 * @param string $prefix
+	 * @param integer $numberOfItems
+	 * @return array
+	 */
+	protected function createItems($prefix, $numberOfItems)
+	{
+		$dm = $this->getDocumentServices()->getDocumentManager();
+		$tm = $this->getApplicationServices()->getTransactionManager();
+		$items = [];
+		for ($i = 0; $i < $numberOfItems; $i++)
+		{
+			$item = $dm->getNewDocumentInstanceByModelName('Rbs_Collection_Item');
+			/* @var $item \Rbs\Collection\Documents\Item */
+			$item->setLabel(ucfirst($prefix) . $i);
+			$item->setValue($prefix . $i);
+			try
+			{
+				$tm->begin();
+				$item->save();
+				$tm->commit();
+				$items[] = $item;
+			}
+			catch (\Exception $e)
+			{
+				$tm->rollBack($e);
+				$this->fail('cannot create item with this error: ' . $e->getMessage());
+			}
+		}
+		return $items;
+	}
 }
---
+++ ChangeTests/Plugins/Modules/Rbs/Collection/Documents/ItemTest.php
@@ -0,0 +1,156 @@
+<?php
+
+class ItemTest extends \ChangeTests\Change\TestAssets\TestCase
+{
+	/**
+	 * @var \Rbs\Collection\Documents\Item
+	 */
+	protected $item;
+
+	public static function setUpBeforeClass()
+	{
+		static::initDocumentsDb();
+	}
+
+	public static function tearDownAfterClass()
+	{
+		static::clearDB();
+	}
+
+	protected function setUp()
+	{
+		parent::setUp();
+		$this->item = $this->createAnItem();
+	}
+
+	protected function tearDown()
+	{
+		$this->deleteAnItem($this->item);
+		parent::tearDown();
+	}
+
+	public function testOnUpdate()
+	{
+		//first try with a non locked item
+		$this->item->setValue('new');
+		$tm = $this->getApplicationServices()->getTransactionManager();
+		try
+		{
+			$tm->begin();
+			$this->item->update();
+			$tm->commit();
+		}
+		catch (\Exception $e)
+		{
+			$tm->rollBack($e);
+			$this->fail('cannot update item with this error: ' . $e->getMessage());
+		}
+		$dm = $this->getDocumentServices()->getDocumentManager();
+		/* @var $item \Rbs\Collection\Documents\Item */
+		$item = $dm->getDocumentInstance($this->item->getId());
+		$this->assertNotNull($item);
+		$this->assertEquals('new', $item->getValue());
+
+		//Now try with a locked item
+		$item = $this->createAnItem(true);
+		$item->setValue('new');
+		try
+		{
+			$tm->begin();
+			$item->update();
+			$tm->commit();
+		}
+		catch (\Exception $e)
+		{
+			$tm->rollBack($e);
+			$this->fail('cannot update item with this error: ' . $e->getMessage());
+		}
+		/* @var $item \Rbs\Collection\Documents\Item */
+		$item = $dm->getDocumentInstance($item->getId());
+		$this->assertNotEquals('new', $item->getValue());
+		//in fact, locked item keep his old value
+		$this->assertEquals('test', $item->getValue());
+	}
+
+	public function testOnDelete()
+	{
+		//first try with a non locked item
+		$tm = $this->getApplicationServices()->getTransactionManager();
+		try
+		{
+			$tm->begin();
+			$this->item->delete();
+			$tm->commit();
+		}
+		catch (\Exception $e)
+		{
+			$tm->rollBack($e);
+			$this->fail('cannot update item with this error: ' . $e->getMessage());
+		}
+
+		//Now try with a locked item
+		$item = $this->createAnItem(true);
+		$this->setExpectedException('\\RuntimeException', 'can not delete locked item');
+		$item->delete();
+	}
+
+	public function testUpdateRestDocumentLink()
+	{
+		$documentLink = new \Change\Http\Rest\Result\DocumentLink(new \Change\Http\UrlManager(new \Zend\Uri\Http()), $this->item, \Change\Http\Rest\Result\DocumentLink::MODE_PROPERTY);
+		$result = $documentLink->toArray();
+		$this->assertArrayHasKey('locked', $result);
+		$this->assertNotNull($result['locked']);
+		$this->assertArrayHasKey('value', $result);
+		$this->assertNotNull($result['value']);
+	}
+
+	/**
+	 * @param boolean $locked
+	 * @return \Rbs\Collection\Documents\Item
+	 */
+	protected function createAnItem($locked = false)
+	{
+		$dm = $this->getDocumentServices()->getDocumentManager();
+		$tm = $this->getApplicationServices()->getTransactionManager();
+
+		$item = $dm->getNewDocumentInstanceByModelName('Rbs_Collection_Item');
+		/* @var $item \Rbs\Collection\Documents\Item */
+		$item->setLabel('Test');
+		$item->setValue('test');
+		$item->setLocked($locked);
+		try
+		{
+			$tm->begin();
+			$item->save();
+			$tm->commit();
+		}
+		catch (\Exception $e)
+		{
+			$tm->rollBack($e);
+			$this->fail('cannot create item with this error: ' . $e->getMessage());
+		}
+		$this->assertTrue($item->getId() > 0);
+		return $item;
+	}
+
+	/**
+	 * @param \Rbs\Collection\Documents\Item $item
+	 */
+	protected function deleteAnItem($item)
+	{
+		$dm = $this->getDocumentServices()->getDocumentManager();
+		$tm = $this->getApplicationServices()->getTransactionManager();
+
+		try
+		{
+			$tm->begin();
+			$item->delete();
+			$tm->commit();
+		}
+		catch (\Exception $e)
+		{
+			$tm->rollBack($e);
+			$this->fail('cannot delete item with this error: ' . $e->getMessage());
+		}
+	}
+}
