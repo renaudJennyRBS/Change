96775c33c3d3000fc54cd2c5c9c2b75d481b9b14
Frédéric Bonjour <frederic.bonjour@rbs.fr>
2013-08-27 10:01:34 +0200

    Refactoring of Editors.

--- Plugins/Modules/Rbs/Collection/Admin/Assets/Collection/editor.js
+++ Plugins/Modules/Rbs/Collection/Admin/Assets/Collection/editor.js
@@ -1,33 +1,28 @@
 (function () {
 
-	function editorChangeCollectionCollection (Editor, FormsManager) {
+	function editorChangeCollectionCollection (EditorManager, REST) {
 
 		return {
+
 			restrict : 'EC',
-
 			templateUrl : 'Rbs/Collection/Collection/editor.twig',
+			replace : false,
+			require : 'rbsDocumentEditor',
 
-			replace: true,
-
-			// Create isolated scope
-			scope: {
-				original: '=document',
-				onSave: '&',
-				onCancel: '&',
-				section: '='
-			},
-
-			link : function (scope, elm, attrs) {
-				Editor.initScope(scope, elm, function(){
+			link : function (scope, element, attrs, editorCtrl) {
+				scope.onReady = function(){
 					if (!angular.isArray(scope.document.items))
 					{
 						scope.document.items = [];
 					}
-				});
+				};
+
+				editorCtrl.init('Rbs_Collection_Collection');
+
 				scope.moveTop = function(index){
 					if (angular.isArray(scope.document.items))
 					{
-						item = scope.document.items.splice(index, 1);
+						var item = scope.document.items.splice(index, 1);
 						scope.document.items.unshift(item[0]);
 					}
 				};
@@ -35,8 +30,8 @@
 				scope.moveUp = function(index){
 					if (angular.isArray(scope.document.items))
 					{
-						d = scope.document.items[index];
-						u = scope.document.items[index-1];
+						var d = scope.document.items[index];
+						var u = scope.document.items[index-1];
 						scope.document.items[index] = u;
 						scope.document.items[index-1] = d;
 					}
@@ -53,8 +48,8 @@
 				scope.moveDown = function(index){
 					if (angular.isArray(scope.document.items))
 					{
-						d = scope.document.items[index+1];
-						u = scope.document.items[index];
+						var d = scope.document.items[index+1];
+						var u = scope.document.items[index];
 						scope.document.items[index + 1] = u;
 						scope.document.items[index] = d;
 					}
@@ -63,16 +58,18 @@
 				scope.remove = function(index){
 					if (angular.isArray(scope.document.items))
 					{
-						item = scope.document.items.splice(index, 1);
+						scope.document.items.splice(index, 1);
 					}
 				};
 
 				scope.cascadeCreateItem = function(){
-					FormsManager.cascade('Rbs/Collection/Item/form.twig', null, function(doc){scope.document.items.push(doc);}, scope.document.label);
+					//EditorManager.cascade('Rbs/Collection/Item/form.twig', null, function(doc){scope.document.items.push(doc);}, scope.document.label);
+					EditorManager.cascade(REST.newResource('Rbs_Collection_Item'), scope.document.label, function(doc){scope.document.items.push(doc);});
 				};
 
 				scope.cascadeEditItem = function(index){
-					FormsManager.cascade('Rbs/Collection/Item/form.twig', {id:scope.document.items[index].id}, function(doc){scope.document.items[index] = doc;}, scope.document.label);
+					//EditorManager.cascade('Rbs/Collection/Item/form.twig', {id:scope.document.items[index].id}, function(doc){scope.document.items[index] = doc;}, scope.document.label);
+					EditorManager.cascade(scope.document.items[index], scope.document.label, function(doc){scope.document.items[index] = doc;});
 				};
 
 			}
@@ -80,8 +77,8 @@
 
 	}
 
-	editorChangeCollectionCollection.$inject = ['RbsChange.Editor', 'RbsChange.FormsManager'];
+	editorChangeCollectionCollection.$inject = ['RbsChange.EditorManager', 'RbsChange.REST'];
 
-	angular.module('RbsChange').directive('editorChangeCollectionCollection', editorChangeCollectionCollection);
+	angular.module('RbsChange').directive('rbsDocumentEditorRbsCollectionCollection', editorChangeCollectionCollection);
 
 })();
