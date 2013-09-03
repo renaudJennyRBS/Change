bcb689866687589fb6aded786d6b97ad7c21782e
Frédéric Bonjour <frederic.bonjour@rbs.fr>
2013-09-03 15:08:48 +0200

    Added 'rbsBoolean' filter.

--- Plugins/Modules/Rbs/Admin/Assets/js/filters/rbschange.filters.js
+++ Plugins/Modules/Rbs/Admin/Assets/js/filters/rbschange.filters.js
@@ -379,6 +379,12 @@
 		};
 	}]);
 
+	app.filter('rbsBoolean', ['RbsChange.i18n', function (i18n) {
+		return function (input) {
+			return i18n.trans(input ? 'm.rbs.admin.admin.js.yes' : 'm.rbs.admin.admin.js.no');
+		};
+	}]);
+
 
 	app.filter('documentProperties', ['$filter', 'RbsChange.Utils', 'RbsChange.REST', 'RbsChange.i18n', function ($filter, Utils, REST, i18n) {
 		var DATE_REGEXP = /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/;
