c11ed1972c5743c28f4cc27f6f33340e7ecb1e38
Frédéric Bonjour <frederic.bonjour@rbs.fr>
2013-09-09 16:12:36 +0200

    Added possibility to easily define the index page of a section.

--- Plugins/Modules/Rbs/Admin/Assets/js/services/query.js
+++ Plugins/Modules/Rbs/Admin/Assets/js/services/query.js
@@ -19,20 +19,36 @@
 			 * @returns {{model: *, where: {and: Array}}}
 			 */
 			function simpleQuery (model, property, value) {
+				var and = [];
+				if (angular.isObject(property) && angular.isUndefined(value)) {
+					angular.forEach(property, function (value, name) {
+						and.push({
+							'op': 'eq',
+							'lexp' : {
+								'property' : name
+							},
+							'rexp' : {
+								'value' : value
+							}
+						});
+					});
+				}
+				else {
+					and.push({
+						'op': 'eq',
+						'lexp' : {
+							'property' : property
+						},
+						'rexp' : {
+							'value' : value
+						}
+					});
+				}
+
 				return {
 					'model' : model,
 					'where' : {
-						'and' : [
-							{
-								'op': 'eq',
-								'lexp' : {
-									'property' : property
-								},
-								'rexp' : {
-									'value' : value
-								}
-							}
-						]
+						'and' : and
 					}
 				};
 			}
