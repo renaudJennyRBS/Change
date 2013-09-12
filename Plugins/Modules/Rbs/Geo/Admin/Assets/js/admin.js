1ecd42d39dcc1625b83096a486545c779a600501
Eric HAUSWALD <eric.hauswald@rbs.fr>
2013-09-12 14:59:49 +0200

    Add new Rbs_Geo_AddressFields document

--- Plugins/Modules/Rbs/Geo/Admin/Assets/js/admin.js
+++ Plugins/Modules/Rbs/Geo/Admin/Assets/js/admin.js
@@ -8,6 +8,7 @@
 	__change.createEditorForModel('Rbs_Geo_Country');
 	__change.createEditorForModel('Rbs_Geo_TerritorialUnit');
 	__change.createEditorForModel('Rbs_Geo_Zone');
+	__change.createEditorForModel('Rbs_Geo_AddressFields');
 
 
 	/**
@@ -18,7 +19,7 @@
 		$provide.decorator('RbsChange.UrlManager', ['$delegate', function ($delegate)
 		{
 			$delegate.routesForLocalizedModels(['Rbs_Geo_Country', 'Rbs_Geo_TerritorialUnit']);
-			$delegate.routesForModels(['Rbs_Geo_Address', 'Rbs_Geo_Zone']);
+			$delegate.routesForModels(['Rbs_Geo_Address', 'Rbs_Geo_Zone', 'Rbs_Geo_AddressFields']);
 			$delegate.model('Rbs_Geo').route('home', 'Rbs/Geo', { 'redirectTo': 'Rbs/Geo/Zone/'});
 			return $delegate;
 		}]);
---
+++ Plugins/Modules/Rbs/Geo/Documents/AddressField.php
@@ -0,0 +1,9 @@
+<?php
+namespace Rbs\geo\Documents;
+
+/**
+ * @name \Rbs\geo\Documents\AddressField
+ */
+class AddressField extends \Compilation\Rbs\Geo\Documents\AddressField
+{
+}
