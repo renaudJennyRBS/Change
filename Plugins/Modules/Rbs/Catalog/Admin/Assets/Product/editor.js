d8f7af91e669861be7e5eae94dc99d2cf11aec25
Eric HAUSWALD <eric.hauswald@rbs.fr>
2013-09-24 15:20:34 +0200

    Refactor catalog product attributes editor

--- Plugins/Modules/Rbs/Catalog/Admin/Assets/Product/editor.js
+++ Plugins/Modules/Rbs/Catalog/Admin/Assets/Product/editor.js
@@ -24,18 +24,17 @@
 					{
 						scope.document.attributeValues = [];
 					}
-				}
+				};
 
 				scope.onReady = function() {
-
-					loadCategorizations();
+					scope.loadCategorizations();
 				};
 
 				scope.attributeGroupId = null;
 
-				editorCtrl.init('Rbs_Catalog_Product');
+				scope.attributesDef = [];
 
-				function loadCategorizations () {
+				scope.loadCategorizations = function() {
 					if (scope.document.META$.links.hasOwnProperty('productcategorizations')) {
 						REST.collection(scope.document.META$.links['productcategorizations'].href).then(function(result){
 							if (angular.isObject(result) && result.hasOwnProperty('resources'))
@@ -47,16 +46,16 @@
 					else {
 						scope.categorizations = [];
 					}
-				}
+				};
 
 				scope.cascadeEditCategorization = function(cat){
-					scope.cascade(cat, scope.document.label, function(doc){loadCategorizations();});
+					scope.cascade(cat, scope.document.label, function(doc){scope.loadCategorizations();});
 				};
 
 				scope.cascadeCreateCategorization = function(doc){
 					var newCat = REST.newResource('Rbs_Catalog_ProductCategorization');
 					newCat.product = scope.document;
-					scope.cascade(newCat, scope.document.label, function(doc){loadCategorizations()});
+					scope.cascade(newCat, scope.document.label, function(doc){scope.loadCategorizations()});
 				};
 
 				scope.toggleHighlight = function(doc){
@@ -74,7 +73,7 @@
 						Loading.start();
 						$http.get(url)
 							.success(function (data) {
-								loadCategorizations();
+								scope.loadCategorizations();
 								Loading.stop();
 							})
 							.error(function errorCallback(data, status) {
@@ -86,35 +85,117 @@
 
 				scope.deleteCategorization = function(doc){
 					REST['delete'](doc).then(function(){
-						loadCategorizations();
+						scope.loadCategorizations();
 					});
 				};
 
+				editorCtrl.init('Rbs_Catalog_Product');
+
 				scope.$watch('document.attribute', function(newValue){
+					var attrGrpId = null;
 					if (newValue)
 					{
 						if (angular.isObject(newValue) && newValue.hasOwnProperty('id'))
 						{
-							scope.attributeGroupId = newValue.id;
+							attrGrpId = newValue.id;
 						}
 						else
 						{
-							var groupId = parseInt(newValue, 10);
-							if (isNaN(groupId))
+							attrGrpId = parseInt(newValue, 10);
+							if (isNaN(attrGrpId))
 							{
-								scope.attributeGroupId = null;
-							}
-							else
-							{
-								scope.attributeGroupId = groupId;
+								attrGrpId = null;
 							}
 						}
 					}
-					else
+					
+					if (attrGrpId != scope.attributeGroupId)
 					{
-						scope.attributeGroupId = null;
+						scope.clearAttributesEditor();
+						scope.attributeGroupId = attrGrpId;
 					}
 				});
+
+				scope.$watch('document.attributeValues', function(newValue){
+					if (angular.isArray(newValue))
+					{
+						scope.assocValues(scope.attributesDef, newValue);
+					}
+				});
+
+				scope.$watch('attributeGroupId', function(newValue){
+					if (newValue)
+					{
+						REST.resource('Rbs_Catalog_Attribute', newValue).then(scope.generateAttributesEditor);
+					}
+				});
+
+				scope.clearAttributesEditor = function (){
+					scope.attributesDef = [];
+					$timeout(function () {
+						scope.$emit('Change:Editor:UpdateMenu');
+					});
+				};
+
+				scope.generateAttributesEditor = function (attribute) {
+					var editorDefinition = attribute.editorDefinition;
+					if (angular.isObject(editorDefinition))
+					{
+						scope.attributesDef = editorDefinition.attributes;
+						if (angular.isArray(scope.document.attributeValues))
+						{
+							scope.assocValues(scope.attributesDef, scope.document.attributeValues);
+						}
+						else
+						{
+							scope.assocValues(scope.attributesDef, []);
+						}
+					}
+
+					$timeout(function () {
+						scope.$emit('Change:Editor:UpdateMenu');
+					});
+				};
+
+				scope.assocValues = function (attributes, attributeValues) {
+					for (var i = 0; i < attributes.length; i++)
+					{
+						if (attributes[i].attributes)
+						{
+							scope.assocValues(attributes[i].attributes, attributeValues)
+						}
+						else
+						{
+							attributes[i].value = scope.getAttributeValue(attributes[i], attributeValues);
+						}
+					}
+				};
+
+				scope.getAttributeValue = function (attribute, attributeValues) {
+					var v = null;
+					for (var i = 0; i < attributeValues.length; i++)
+					{
+						v = attributeValues[i];
+						if (v.id == attribute.id)
+						{
+							if (v.value === null && attribute.valueType == 'Property' && 'propertyName' in attribute)
+							{
+								v.value = scope.document[attribute.propertyName];
+							}
+							return v;
+						}
+					}
+
+					var defaultValue = attribute.defaultValue;
+					if (attribute.valueType == 'Property' && 'propertyName' in attribute)
+					{
+						defaultValue = scope.document[attribute.propertyName];
+					}
+
+					v = {id: attribute.id, valueType: attribute.valueType, value: defaultValue};
+					attributeValues.push(v);
+					return v;
+				};
 			}
 		};
 	}
