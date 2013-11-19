6daf488fda8b39aec528df77f7911afb41464370
Maximilien Augier <maximilien.augier@rbs.fr>
2013-11-19 15:29:19 +0100

    [Fix]Admin/Assets to Assets/Admin for Brand and Catalog plugins
    [Fix]Various locales

--- Plugins/Modules/Rbs/Catalog/Assets/Admin/admin.js
+++ Plugins/Modules/Rbs/Catalog/Assets/Admin/admin.js
@@ -28,22 +28,19 @@
 		$provide.decorator('RbsChange.UrlManager', ['$delegate', function ($delegate)
 		{
 			$delegate.model('Rbs_Catalog_Product')
-				.route('prices', 'Rbs/Catalog/Product/:id/Prices/', 'Rbs/Catalog/Product/product-prices.twig')
-				.route('cross-selling-lists', 'Rbs/Catalog/Product/:id/CrossSellingProductLists/', 'Rbs/Catalog/Product/product-cross-selling.twig')
-				.route('variant-group', 'Rbs/Catalog/Product/:id/VariantGroup/', 'Rbs/Catalog/VariantGroup/list.twig');
+				.route('prices', 'Rbs/Catalog/Product/:id/Prices/', 'Document/Rbs/Catalog/Product/product-prices.twig')
+				.route('cross-selling-lists', 'Rbs/Catalog/Product/:id/CrossSellingProductLists/', 'Document/Rbs/Catalog/Product/product-cross-selling.twig')
+				.route('variant-group', 'Rbs/Catalog/Product/:id/VariantGroup/', 'Document/Rbs/Catalog/VariantGroup/list.twig');
 
 			$delegate.model('Rbs_Catalog_ProductList')
-				.route('productListItems', 'Rbs/Catalog/ProductList/:id/ProductListItem/', 'Rbs/Catalog/ProductList/products.twig');
+				.route('productListItems', 'Rbs/Catalog/ProductList/:id/ProductListItem/', 'Document/Rbs/Catalog/ProductList/products.twig');
 			$delegate.model('Rbs_Catalog_SectionProductList')
-				.route('productListItems', 'Rbs/Catalog/SectionProductList/:id/ProductListItem/', 'Rbs/Catalog/ProductList/products.twig');
+				.route('productListItems', 'Rbs/Catalog/SectionProductList/:id/ProductListItem/', 'Document/Rbs/Catalog/ProductList/products.twig');
 			$delegate.model('Rbs_Catalog_CrossSellingProductList')
-				.route('productListItems', 'Rbs/Catalog/CrossSellingProductList/:id/ProductListItem/', 'Rbs/Catalog/ProductList/products.twig');
+				.route('productListItems', 'Rbs/Catalog/CrossSellingProductList/:id/ProductListItem/', 'Document/Rbs/Catalog/ProductList/products.twig');
 
 			$delegate.model('Rbs_Catalog')
 				.route('home', 'Rbs/Catalog', { 'redirectTo': 'Rbs/Catalog/Product/'});
-
-			/*$delegate.model('Rbs_Catalog_VariantGroup')
-				.route('product-variant-group', 'Rbs/Catalog/Product/:product/VariantGroup/:id', 'Rbs/Catalog/VariantGroup/editor.twig');*/
 
 			$delegate.routesForLocalizedModels(['Rbs_Catalog_Product', 'Rbs_Catalog_Attribute']);
 			$delegate.routesForModels(['Rbs_Catalog_ProductList', 'Rbs_Catalog_SectionProductList', 'Rbs_Catalog_CrossSellingProductList',
@@ -55,7 +52,7 @@
 	app.service('RbsChange.ProductListService', ['RbsChange.MainMenu', function (MainMenu) {
 		return {
 			'addListContent' : function (scope) {
-				MainMenu.addAsideTpl('productlist-content', 'Rbs/Catalog/ProductList/productlist-content-aside-menu.twig', scope);
+				MainMenu.addAsideTpl('productlist-content', 'Document/Rbs/Catalog/ProductList/productlist-content-aside-menu.twig', scope);
 				return MainMenu;
 			}
 		};
