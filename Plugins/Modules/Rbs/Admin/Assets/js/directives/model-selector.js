8a8b7be3273bfcb8e033d0425b860a889b00c488
Gaël PORT <gael.port@rbs.fr>
2013-11-20 10:35:34 +0100

    [NEW] Allow to specify an array of model names in select-model attribute of a picker.

--- Plugins/Modules/Rbs/Admin/Assets/js/directives/model-selector.js
+++ Plugins/Modules/Rbs/Admin/Assets/js/directives/model-selector.js
@@ -4,10 +4,10 @@
 
 	var app = angular.module('RbsChange');
 
-	app.directive('rbsModelSelector', ['RbsChange.Models', function (Models) {
+	app.directive('rbsModelSelector', ['RbsChange.Models', 'RbsChange.ArrayUtils', function (Models, ArrayUtils) {
 		return {
 			restrict : 'E',
-			template : '<select ng-model="model" ng-options="m.label group by m.plugin for m in models | filter:filter | orderBy:[\'plugin\',\'label\']"></select>',
+			template : '<select ng-model="model" ng-options="m.label group by m.plugin for m in models | filter:filter:comparator | orderBy:[\'plugin\',\'label\']"></select>',
 			replace  : true,
 
 			scope : {
@@ -18,6 +18,18 @@
 			link : function (scope, elm, attrs) {
 				scope.models = Models.getAll();
 				scope.filter = {};
+				scope.comparator = function (expected, actual)
+				{
+					if (angular.isArray(actual))
+					{
+						return ArrayUtils.inArray(expected, actual) != -1;
+					}
+					else if (actual === null)
+					{
+						return true;
+					}
+					return expected == actual;
+				};
 				attrs.$observe('filter', function (value) {
 					scope.filter = scope.$eval(attrs.filter);
 				});
