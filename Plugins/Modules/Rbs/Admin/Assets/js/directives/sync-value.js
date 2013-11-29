b1e3563c58f1a8a7df5dbd307d33c11fd6f22062
Eric HAUSWALD <eric.hauswald@rbs.fr>
2013-11-29 12:41:58 +0100

    Fix angular rbs-sync-value directive.

--- Plugins/Modules/Rbs/Admin/Assets/js/directives/sync-value.js
+++ Plugins/Modules/Rbs/Admin/Assets/js/directives/sync-value.js
@@ -2,7 +2,7 @@
 
 	"use strict";
 
-	angular.module('RbsChange').directive('rbsSyncValue', ['$parse', '$compile', '$timeout', function ($parse, $compile, $timeout)
+	angular.module('RbsChange').directive('rbsSyncValue', ['$parse', '$compile', function ($parse, $compile)
 	{
 		return {
 			restrict : 'A',
@@ -15,13 +15,16 @@
 			{
 				if (!ngModel) return;
 
-				var	source = attrs.rbsSyncValue,
-					originalValue,
+				var	source = attrs.rbsSyncValue, originalValue,
 					targetSetterFn = $parse(attrs.ngModel).assign,
-					unwatchSourceFn,
-					renderFn = ngModel.$render;
+					unwatchSourceFn;
 
 				scope.enabled = false;
+
+				ngModel.$formatters.unshift(function (value) {
+					scope.enabled = (value === scope.$eval(attrs.rbsSyncValue));
+					return value;
+				});
 
 				$compile('<button class="btn sync-value-button" type="button" ng-click="enabled = ! enabled" ng-class="{\'btn-success\': enabled}"><span ng-if="enabled">=</span><span ng-if="! enabled">&ne;</span></button>')(scope, function (clone) {
 					clone.attr('title', attrs.syncTitle);
@@ -31,49 +34,35 @@
 				elm.parent().css({'position': 'relative'});
 
 
-				scope.$watch('enabled', function (enabled, old)
+				scope.$watch('enabled', function (enabled)
 				{
-					if (angular.isDefined(enabled) && enabled !== old)
+					if (angular.isDefined(enabled))
 					{
+						if (unwatchSourceFn) {
+							unwatchSourceFn();
+						}
 						if (enabled) {
 							originalValue = angular.copy(ngModel.$viewValue);
 							elm.attr('readonly', 'readonly');
 							unwatchSourceFn = scope.$watch(source, function (value) {
-								ngModel.$setViewValue(value);
-								ngModel.$render();
+								setValue(value);
 							});
-							ngModel.$setViewValue(scope.$eval(attrs.rbsSyncValue));
-							ngModel.$render();
+							setValue(scope.$eval(attrs.rbsSyncValue));
 						} else {
 							elm.removeAttr('readonly');
-							if (unwatchSourceFn) {
-								unwatchSourceFn();
+							if (originalValue) {
+								targetSetterFn(scope, originalValue);
+								elm.focus();
 							}
-							targetSetterFn(scope, originalValue);
-							elm.focus();
 						}
 					}
 				}, true);
 
-				var initialValueSet = false;
-
-				ngModel.$render = function ()
+				function setValue(value)
 				{
-					if (angular.isDefined(ngModel.$viewValue) && ! initialValueSet) {
-						scope.enabled = ! ngModel.$viewValue || angular.equals(ngModel.$viewValue, scope.$eval(attrs.rbsSyncValue));
-						initialValueSet = true;
-					}
-					renderFn();
-				};
-
-/*
-				$timeout(function () {
-					originalValue = angular.copy(ngModel.$viewValue);
-					console.log("originalValue=", originalValue);
-					enabled = ! ngModel.$viewValue || angular.equals(ngModel.$viewValue, scope.$eval(attrs.rbsSyncValue));
-					update();
-				});
-*/
+					ngModel.$setViewValue(value);
+					ngModel.$render();
+				}
 			}
 		};
 	}]);
