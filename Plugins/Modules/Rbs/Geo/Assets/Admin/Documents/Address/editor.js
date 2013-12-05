191d825659a40db0de634617f2220d4a2803868b
michal OLEXA <michal.olexa@rbs.fr>
2013-12-05 17:01:00 +0100

    [FIX] geo address BO editor

--- Plugins/Modules/Rbs/Geo/Assets/Admin/Documents/Address/editor.js
+++ Plugins/Modules/Rbs/Geo/Assets/Admin/Documents/Address/editor.js
@@ -5,12 +5,11 @@
 	/**
 	 * @param $timeout
 	 * @param $http
-	 * @param Loading
 	 * @param REST
 	 * @param EditorManager
 	 * @constructor
 	 */
-	function Editor($timeout, $http, Loading, REST, EditorManager)
+	function Editor($timeout, $http, REST, EditorManager)
 	{
 		return {
 			restrict : 'C',
@@ -117,6 +116,6 @@
 		};
 	}
 
-	Editor.$inject = ['$timeout', '$http', 'RbsChange.Loading', 'RbsChange.REST', 'RbsChange.EditorManager'];
+	Editor.$inject = ['$timeout', '$http', 'RbsChange.REST', 'RbsChange.EditorManager'];
 	angular.module('RbsChange').directive('rbsDocumentEditorRbsGeoAddress', Editor);
 })();
