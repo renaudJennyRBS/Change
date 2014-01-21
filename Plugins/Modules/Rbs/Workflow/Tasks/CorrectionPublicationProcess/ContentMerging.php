8b2c86475bc30fe82eae397765a7b1e18c80a38b
Eric HAUSWALD <eric.hauswald@rbs.fr>
2014-01-21 10:27:12 +0100

    Fix \Rbs\Workflow\Job\DocumentCleanUp::onCorrectionFiled
    Add new constant: \Change\Workflow\Interfaces\WorkItem::CORRECTION_ID_CONTEXT_KEY

--- Plugins/Modules/Rbs/Workflow/Tasks/CorrectionPublicationProcess/ContentMerging.php
+++ Plugins/Modules/Rbs/Workflow/Tasks/CorrectionPublicationProcess/ContentMerging.php
@@ -23,7 +23,7 @@ class ContentMerging
 		if ($document instanceof Correction)
 		{
 			$correction = $document->getCurrentCorrection();
-			if ($correction && $correction->getId() == $ctx['__CORRECTION_ID'])
+			if ($correction && $correction->getId() == $ctx[WorkItem::CORRECTION_ID_CONTEXT_KEY])
 			{
 				$applicationServices = $event->getApplicationServices();
 				$transactionManager = $applicationServices->getTransactionManager();
