f1b461cbcdf6f7821610a1cf5985e7a7ac2a730a
Renaud Jenny <renaud.jenny@rbs.fr>
2014-01-22 10:24:45 +0100

    Module Mail UI

---
+++ Plugins/Modules/Rbs/Mail/Assets/Admin/admin.js
@@ -0,0 +1,22 @@
+(function () {
+
+	"use strict";
+
+	var app = angular.module('RbsChange');
+
+	__change.createEditorForModel('Rbs_Mail_Mail');
+
+	app.config(['$provide', function ($provide)
+	{
+		$provide.decorator('RbsChange.UrlManager', ['$delegate', function ($delegate)
+		{
+			$delegate.module('Rbs_Mail', 'Rbs/Mail', { 'redirectTo': 'Rbs/Mail/Mail/'});
+
+			$delegate.routesForModels([
+				'Rbs_Mail_Mail'
+			]);
+			return $delegate.module(null);
+		}]);
+	}]);
+
+})();
