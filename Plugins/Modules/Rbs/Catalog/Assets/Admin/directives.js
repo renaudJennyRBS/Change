1c3302deea403f9640c7f3a250cdbcc5e6f8b771
Gaël PORT <gael.port@rbs.fr>
2014-02-21 11:50:45 +0100

    Fix visuals in product aside preview.

--- Plugins/Modules/Rbs/Catalog/Assets/Admin/directives.js
+++ Plugins/Modules/Rbs/Catalog/Assets/Admin/directives.js
@@ -5,19 +5,19 @@
 	var app = angular.module('RbsChange');
 
 
-	app.directive('rbsDocumentPreviewRbsCatalogProduct', function ()
+	app.directive('rbsDocumentPreviewRbsCatalogProduct', ['RbsChange.REST', function (REST)
 	{
 		return {
 			restrict : 'E',
 			scope : {
 				document : '='
 			},
-			template :
-				'<p><a href="(= document | rbsURL =)"><strong>(= document.label =)</strong><br/><small>(= document.sku.code =)</small></a></p>' +
-				'<p><img rbs-storage-image="document.visuals[0].id" thumbnail="M"/></p>' +
-				'<p><img ng-repeat="v in document.visuals" ng-if="! $first" rbs-storage-image="v.id" thumbnail="S"/></p>'
+			templateUrl : 'Rbs/Catalog/rbs-document-preview-rbs-catalog-product.twig',
+			link: function(scope) {
+				REST.ensureLoaded(scope.document);
+			}
 		};
-	});
+	}]);
 
 	app.directive('rbsDocumentFilterStockInventory', function() {
 		return {
