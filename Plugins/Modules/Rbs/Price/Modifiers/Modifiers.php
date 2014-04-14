54a26d33bb41ac758205610ba609deabf00ac59c
Eric Hauswald <eric.hauswald@gmail.com>
2014-04-14 15:28:18 +0200

    Add Lines amount Price modifier.

--- Plugins/Modules/Rbs/Price/Modifiers/Modifiers.php
+++ Plugins/Modules/Rbs/Price/Modifiers/Modifiers.php
@@ -23,7 +23,8 @@ class Modifiers
 		$applicationServices = $event->getApplicationServices();
 		$i18nManager = $applicationServices->getI18nManager();
 		$collection = array(
-			'rbs-price-modifier-reservation-quantity' => new I18nString($i18nManager, 'm.rbs.price.admin.modifiers_reservation_quantity_title', array('ucf'))
+			'rbs-price-modifier-reservation-quantity' => new I18nString($i18nManager, 'm.rbs.price.admin.modifiers_reservation_quantity_title', array('ucf')),
+			'rbs-price-modifier-lines-amount' => new I18nString($i18nManager, 'm.rbs.price.admin.modifiers_lines_amount_title', array('ucf'))
 		);
 		$collection = new \Change\Collection\CollectionArray('Rbs_Price_Collection_ModifierNames', $collection);
 		$event->setParam('collection', $collection);
@@ -62,6 +63,52 @@ class Modifiers
 						if (isset($threshold['l']) && isset($threshold['v']) && $quantity >= $threshold['l'])
 						{
 								$value = $threshold['v'];
+						}
+					}
+					if ($value !== false)
+					{
+						$event->setParam('contextualValue', $value);
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * Event Param: cart, order
+	 * @param \Change\Events\Event $event
+	 */
+	public function onPriceModifierLinesAmount(\Change\Events\Event $event)
+	{
+		$price = $event->getTarget();
+		if ($price instanceof \Rbs\Price\Documents\Price)
+		{
+			$options = $price->getOptions();
+			if (isset($options['thresholds']) && is_array($options['thresholds']) && count($options['thresholds']))
+			{
+
+				$container = $event->getParam('cart', $event->getParam('order'));
+				if ($container instanceof \Rbs\Commerce\Cart\Cart)
+				{
+					$amount = ($container->getPricesValueWithTax()) ? $container->getLinesAmountWithTaxes() : $container->getLinesAmount();
+				}
+				elseif ($container instanceof \Rbs\Order\Documents\Order)
+				{
+					$amount = ($container->getPricesValueWithTax()) ? $container->getLinesAmountWithTaxes() : $container->getLinesAmount();
+				}
+				else
+				{
+					$amount = null;
+				}
+
+				if ($amount !== null)
+				{
+					$value = false;
+					foreach ($options['thresholds'] as $threshold)
+					{
+						if (isset($threshold['l']) && isset($threshold['v']) && $amount >= $threshold['l'])
+						{
+							$value = $threshold['v'];
 						}
 					}
 					if ($value !== false)
