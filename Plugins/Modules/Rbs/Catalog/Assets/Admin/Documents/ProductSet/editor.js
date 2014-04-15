03bb433e713aa16f5cb97d6e177b5fbc7a15b3a4
Renaud Jenny <renaud.jenny@gmail.com>
2014-04-15 11:06:45 +0200

    Fix issue in new ProductSet editor

--- Plugins/Modules/Rbs/Catalog/Assets/Admin/Documents/ProductSet/editor.js
+++ Plugins/Modules/Rbs/Catalog/Assets/Admin/Documents/ProductSet/editor.js
@@ -11,9 +11,9 @@
 
 			link: function(scope) {
 				scope.onReady = function () {
-					if ($routeParams.productId) {
+					if ($routeParams.rootProductId) {
 						//Creation : get Product
-						REST.resource('Rbs_Catalog_Product', $routeParams.productId).then(function (product) {
+						REST.resource('Rbs_Catalog_Product', $routeParams.rootProductId).then(function (product) {
 							scope.document.rootProduct = product;
 						});
 					}
