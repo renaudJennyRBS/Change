8d646ae0f9fd0e74fe7d4475c168e250c0c30c06
Gaël PORT <gael.port@rbs.fr>
2014-07-08 17:14:19 +0200

    Delete unused contentType block parameter.

--- Plugins/Modules/Rbs/Website/Blocks/RichtextInformation.php
+++ Plugins/Modules/Rbs/Website/Blocks/RichtextInformation.php
@@ -8,8 +8,6 @@
  */
 namespace Rbs\Website\Blocks;
 
-use Change\Documents\Property;
-
 /**
  * @name \Rbs\Website\Blocks\RichtextInformation
  */
@@ -22,6 +20,5 @@ class RichtextInformation extends \Change\Presentation\Blocks\Information
 		$ucf = array('ucf');
 		$this->setSection($i18nManager->trans('m.rbs.website.admin.module_name', $ucf));
 		$this->setLabel($i18nManager->trans('m.rbs.website.admin.richtext', $ucf));
-		$this->addInformationMeta('contentType', Property::TYPE_STRING, true, 'Markdown');
 	}
 }
