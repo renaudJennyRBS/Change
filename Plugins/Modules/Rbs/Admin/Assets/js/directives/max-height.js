7a37753fd3ca8d6829401eadd8085b9fa1013e6f
Gaël PORT <gael.port@proximis.com>
2014-11-19 16:58:55 +0100

    Add new max-height directive in back office.

---
+++ Plugins/Modules/Rbs/Admin/Assets/js/directives/max-height.js
@@ -0,0 +1,61 @@
+/**
+ * Copyright (C) 2014 Proximis
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+(function() {
+	"use strict";
+	var app = angular.module('RbsChange');
+
+	function rbsMaxHeight() {
+		return {
+			restrict: 'A',
+			templateUrl: 'Rbs/Admin/js/directives/max-height.twig',
+			transclude: true,
+			scope: {},
+			link: function(scope, elm, attrs) {
+				scope.containerNode = elm.find('.max-height-container');
+				scope.contentNode = elm.find('.max-height-content');
+				scope.deployed = false;
+				scope.showButtons = false;
+				scope.maxHeight = parseInt(attrs['rbsMaxHeight'], 10);
+				if (isNaN(scope.maxHeight) || scope.maxHeight < 0) {
+					scope.maxHeight = 0;
+				}
+				console.log(angular.copy(scope.maxHeight));
+
+				scope.toggle = function() {
+					scope.deployed = !scope.deployed;
+					refreshStyles();
+				};
+
+				function refreshStyles() {
+					if (!scope.showButtons || scope.deployed) {
+						scope.containerNode.css({ overflow: 'visible', 'max-height': "" });
+					}
+					else {
+						scope.containerNode.css({ overflow: 'hidden', 'max-height': scope.maxHeight + 'px' });
+					}
+				}
+
+				scope.showButtonsFunction = function showButtonsFunction() {
+					if (!scope.maxHeight) {
+						scope.showButtons = false;
+					}
+					else if (scope.contentNode.height() > scope.maxHeight + 20) {
+						scope.showButtons = true;
+					}
+					else {
+						scope.showButtons = false;
+					}
+					refreshStyles();
+					return scope.showButtons;
+				};
+			}
+		}
+	}
+
+	app.directive('rbsMaxHeight', rbsMaxHeight);
+})();
