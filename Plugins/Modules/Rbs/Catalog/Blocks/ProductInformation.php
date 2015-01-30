af338903508969ed531e160686164e151d567747
Gaël PORT <gael.port@proximis.com>
2015-01-30 17:40:49 +0100

    Handle social buttons in product detail.

--- Plugins/Modules/Rbs/Catalog/Blocks/ProductInformation.php
+++ Plugins/Modules/Rbs/Catalog/Blocks/ProductInformation.php
@@ -40,10 +40,19 @@ class ProductInformation extends Information
 			->setLabel($i18nManager->trans('m.rbs.catalog.admin.product_common_information', $ucf))
 			->setAllowedModelsNames('Rbs_Website_Text');
 
+		// Default template.
 		$templateInformation = $this->addDefaultTemplateInformation();
 		$templateInformation->addParameterInformation('activateZoom', Property::TYPE_BOOLEAN, false, true)
 			->setLabel($i18nManager->trans('m.rbs.catalog.admin.product_activate_zoom', $ucf));
 
+		$socialModule = $event->getApplicationServices()->getPluginManager()->getModule('Rbs', 'Social');
+		if ($socialModule && $socialModule->isAvailable())
+		{
+			$templateInformation->addParameterInformation('handleSocial', Property::TYPE_BOOLEAN, false, true)
+				->setLabel($i18nManager->trans('m.rbs.catalog.admin.product_handle_social', $ucf));
+		}
+
+		// Template with reviews.
 		$templateInformation = $this->addTemplateInformation('Rbs_Catalog', 'product-with-reviews.twig');
 		$templateInformation->setLabel($i18nManager->trans('m.rbs.catalog.admin.product_with_reviews_label', ['ucf']));
 		$templateInformation->addParameterInformation('activateZoom', Property::TYPE_BOOLEAN, false, true)
@@ -58,6 +67,13 @@ class ProductInformation extends Information
 		$templateInformation->addParameterInformation('handleReviewVotes', Property::TYPE_BOOLEAN, false, true)
 			->setLabel($i18nManager->trans('m.rbs.review.admin.parameter_handle_review_votes', $ucf));
 
+		if ($socialModule && $socialModule->isAvailable())
+		{
+			$templateInformation->addParameterInformation('handleSocial', Property::TYPE_BOOLEAN, false, true)
+				->setLabel($i18nManager->trans('m.rbs.catalog.admin.product_handle_social', $ucf));
+		}
+
+		// Compact template.
 		$templateInformation = $this->addTemplateInformation('Rbs_Catalog', 'product-compact.twig');
 		$templateInformation->setLabel($i18nManager->trans('m.rbs.catalog.admin.product_compact_label', ['ucf']));
 		$templateInformation->addParameterInformation('imageFormats', Property::TYPE_STRING, false, 'x,detailCompact,pictogram,attribute')
